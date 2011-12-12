3595ee85463cb0c06a6aa0f6ad765bd9613585ef
Andy Gregorowicz <andy@mitre.org>
2011-12-12 15:59:25 -0500

    Switching reason to an embedded document since it is a full Entry

--- lib/health-data-standards/models/encounter.rb
+++ lib/health-data-standards/models/encounter.rb
@@ -1,7 +1,7 @@
 class Encounter < Entry
   field :performer, type: Hash
   field :facility, type: Hash
-  field :reason, type: Hash
   field :admit_type, type: Hash, label: 'admitType'
   field :discharge_disp, type: Hash, label: 'dischargeDisp'
+  embeds_one :reason, class_name: "Entry"
 end
