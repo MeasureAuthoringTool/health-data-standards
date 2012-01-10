1cb8f7c4bac325cfd5b1e0d678449f76740d2030
Adam Goldstein <agoldstein@mitre.org>
2012-01-10 10:07:01 -0500

    Adding aliases to Encounters

--- lib/health-data-standards/models/encounter.rb
+++ lib/health-data-standards/models/encounter.rb
@@ -1,7 +1,10 @@
 class Encounter < Entry
   field :performer, type: Hash
   field :facility, type: Hash
-  field :admitType, type: Hash, as: 'admit_type'
-  field :dischargeDisp, type: Hash, as: 'discharge_disp'
+  field :admitType, type: Hash
+  field :dischargeDisp, type: Hash
   embeds_one :reason, class_name: "Entry"
+  
+  alias admitType, admit_type
+  alias dischargeDisp, discharge_disp
 end
