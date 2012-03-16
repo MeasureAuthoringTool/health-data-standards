72bb495ca6759acad843d8a0a53c7a4b7a4d1ff6
Andy Gregorowicz <andy@mitre.org>
2012-03-16 13:15:36 -0400

    Implementing the conditions section importer for the HITSP C32

---
+++ test/unit/import/c32/condition_importer_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class AllergyImporterTest < MiniTest::Unit::TestCase
+  def test_condition_importing
+    doc = Nokogiri::XML(File.new('test/fixtures/NISTExampleC32.xml'))
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    pi = HealthDataStandards::Import::C32::PatientImporter.instance
+    patient = pi.parse_c32(doc)
+
+    condition = patient.conditions[0]
+
+    assert_equal 'Condition', condition.type
+    assert ! condition.cause_of_death
+    assert condition.codes['SNOMED-CT'].include? '195967001'
+    assert_equal Time.gm(1950).to_i, condition.start_time
+
+  end
+end
