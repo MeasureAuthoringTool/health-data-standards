73611bfa6afb0f96bd1911719dd8538538e72e57
John Mazella <jmazella>
2012-06-08 15:09:47 -0400

    Fixing patient csv export to output race and ethnicity correctly

--- lib/health-data-standards/export/csv.rb
+++ lib/health-data-standards/export/csv.rb
@@ -17,7 +17,9 @@ module HealthDataStandards
       end
       
       def extract_patient_data(patient)
-       [patient.patient_id, patient.first, patient.last, patient.gender,patient.race,patient.ethnicity,Time.at(patient.birthdate).strftime('%m/%d/%Y')]
+        race = patient.race['name'] || patient.race
+        ethnicity = patient.ethnicity['name'] || patient.ethnicity
+       [patient.patient_id, patient.first, patient.last, patient.gender, race, ethnicity, Time.at(patient.birthdate).strftime('%m/%d/%Y')]
       end
 
       extend self
