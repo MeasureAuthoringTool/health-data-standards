2b3c4d20a074e2e21c9d1c856d9fc1b5edf142f2
Andy Gregorowicz <andy@mitre.org>
2012-07-31 13:51:54 -0400

    Switching the storage of clinical trial participant from snake case to camel case so that it matches the rest of the models/patient API

--- lib/health-data-standards/models/record.rb
+++ lib/health-data-standards/models/record.rb
@@ -16,7 +16,7 @@ class Record
   field :marital_status, type: Hash # TODO
   field :medical_record_number, type: String
   field :expired, type: Boolean
-  field :clinical_trial_participant, type: Boolean # Currently not implemented in the C32 importer
+  field :clinicalTrialParticipant, type: Boolean   # Currently not implemented in the C32 importer
                                                    # because it cannot be easily represented in a
                                                    # HITSP C32
 
@@ -50,6 +50,9 @@ class Record
   def over_18?
     Time.at(birthdate) < Time.now.years_ago(18)
   end
+  
+  alias :clinical_trial_participant :clinicalTrialParticipant
+  alias :clinical_trial_participant= :clinicalTrialParticipant=
   
   private 
   
