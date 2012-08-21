0d7bdd630b3c6730de2b06949554ad61d0607c7c
Samuel Sayer <ssayer@mitre.org>
2012-08-21 16:39:50 -0400

    fix schema issues, create sample greenc32 record

--- lib/health-data-standards/models/encounter.rb
+++ lib/health-data-standards/models/encounter.rb
@@ -1,12 +1,13 @@
 class Encounter < Entry
   field :admitType, type: Hash
   field :dischargeDisposition, type: Hash
-  
+  field :admit_time, type: Integer
+  field :discharge_time, type: Integer
   embeds_one :facility, class_name: "Organization"
   embeds_one :reason, class_name: "Entry"
 
   belongs_to :performer, class_name: "Provider"
-
+  
   alias :admit_type :admitType
   alias :admit_type= :admitType=
   alias :discharge_disposition :dischargeDisposition
