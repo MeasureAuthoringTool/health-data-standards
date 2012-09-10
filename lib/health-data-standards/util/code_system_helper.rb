4c9cbbfaf2b4927f3d957f1fc6f3c2678305ae46
Andre Quina <aquina@mitre.org>
2012-09-10 10:43:04 -0400

    added code system for severity in hqmf

--- lib/health-data-standards/util/code_system_helper.rb
+++ lib/health-data-standards/util/code_system_helper.rb
@@ -17,7 +17,8 @@ module HealthDataStandards
         '2.16.840.1.113883.3.26.1.1' => 'NCI Thesaurus',
         '2.16.840.1.113883.3.88.12.80.20' => 'FDA',
         '2.16.840.1.113883.5.14' => 'HL7 ActStatus',
-        '2.16.840.1.113883.6.259' => 'HL7 Healthcare Service Location'
+        '2.16.840.1.113883.6.259' => 'HL7 Healthcare Service Location',
+        '2.16.840.1.113883.5.4' => 'HL7 Act Code'
       }
       
       # Returns the name of a code system given an oid
