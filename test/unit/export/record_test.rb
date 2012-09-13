657b50a5d2bb4870959a33ad9fb6bac2ea9b458e
Samuel Sayer <ssayer@mitre.org>
2012-09-13 13:27:01 -0400

    clean up medications template

--- test/unit/export/record_test.rb
+++ test/unit/export/record_test.rb
@@ -1,16 +1,27 @@
 require 'test_helper'
 require 'pry'
+require "fileutils"
 
 module GreenC32
   class RecordTest < MiniTest::Unit::TestCase
 
     def setup
-      collection_fixtures('records', '_id')
-      @record = Record.first
+      
     end
     
     def test_export
-      HealthDataStandards::Export::GreenC32::Record.export(@record)
+      # Dir.new("c32").each do |c32|
+      # 	if c32.ends_with?("xml")
+      #     doc = Nokogiri::XML(File.open("c32/#{c32}"))
+      #     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+      # 	  record = HealthDataStandards::Import::C32::PatientImporter.instance.parse_c32(doc)
+      # 	  binding.pry
+      #     green = HealthDataStandards::Export::GreenC32::Record.export(record)
+      #     FileUtils.touch("greenc32/#{c32}")
+      #     File.open("greenc32/#{c32}", "w") { |f| f.write(green) }
+      # 	end
+      # end
+      # HealthDataStandards::Export::GreenC32::Record.export(@record)
     end
   end
 end
