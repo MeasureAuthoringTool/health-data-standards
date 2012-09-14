7df5a3799eb17d884099294344d8bdec1922c01c
Marc Hadley <mhadley@mitre.org>
2012-09-14 09:56:19 -0400

    Add ordinality_code element - a hash of codeset => [codes] same as status_code

--- lib/health-data-standards/models/condition.rb
+++ lib/health-data-standards/models/condition.rb
@@ -1,10 +1,11 @@
 class Condition < Entry
-  field :type,          type: String
-  field :causeOfDeath,  type: Boolean
-  field :priority,      type: Integer
-  field :name,          type: String
-  field :ordinality,    type: String
-  field :severity,      type: Hash # Currently unsupported by any importers
+  field :type,                type: String
+  field :causeOfDeath,        type: Boolean
+  field :priority,            type: Integer
+  field :name,                type: String
+  field :ordinality,          type: String
+  field :ordinality_code,     type: Hash
+  field :severity,            type: Hash # Currently unsupported by any importers
 
   embeds_many :treating_provider,  class_name: "Provider"
   
