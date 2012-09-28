e8f05bda0eb60faad29536c5401933b7dd80fa53
Andy Gregorowicz <andy@mitre.org>
2012-09-28 09:20:59 -0400

    Adding anatomical structure

--- lib/health-data-standards/models/medical_equipment.rb
+++ lib/health-data-standards/models/medical_equipment.rb
@@ -1,5 +1,8 @@
 class MedicalEquipment < Entry
   
   field :manufacturer, type: String
-   
+  field :anatomicalStructure, type: Hash
+
+  alias :anatomical_structure :anatomicalStructure
+  alias :anatomical_structure= :anatomicalStructure=
 end
