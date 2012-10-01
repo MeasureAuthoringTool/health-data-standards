e850a3a82ae9f8c04caf4fdfaf29f1e370e9454e
Andy Gregorowicz <andy@mitre.org>
2012-10-01 09:24:31 -0400

    Adding incisionDateTime

--- lib/health-data-standards/models/procedure.rb
+++ lib/health-data-standards/models/procedure.rb
@@ -1,5 +1,6 @@
 class Procedure < Entry
-  field :site,          type: Hash
+  field :site,             type: Hash
+  field :incisionDateTime, type: Integer
   
   belongs_to :performer, class_name: "Provider"
 end
