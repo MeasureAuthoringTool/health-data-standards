71867203819f57f6d962e15b9e619343a68e1769
Andy Gregorowicz <andy@mitre.org>
2012-10-01 10:25:42 -0400

    Adding in the aliases for idiomatic ruby access to the incision date time

--- lib/health-data-standards/models/procedure.rb
+++ lib/health-data-standards/models/procedure.rb
@@ -3,4 +3,7 @@ class Procedure < Entry
   field :incisionDateTime, type: Integer
   
   belongs_to :performer, class_name: "Provider"
+
+  alias :incision_date_time :incisionDateTime
+  alias :incision_date_time= :incisionDateTime=
 end
