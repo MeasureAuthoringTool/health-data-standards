0e97552c146c9acb65e03b1b6d3563e889abff19
Andre Quina <aquina@mitre.org>
2012-10-09 14:34:26 -0400

    add mouse over to patient html export to show matching concepts

--- lib/health-data-standards/export/html.rb
+++ lib/health-data-standards/export/html.rb
@@ -3,10 +3,10 @@ module HealthDataStandards
     module HTML
       include TemplateHelper
 
-      def export(patient)
+      def export(patient, concept_map=nil)
         self.template_format = "html"
         self.template_subdir = "html"
-        render(:template => 'show', :locals => {:patient => patient})
+        render(:template => 'show', :locals => {:patient => patient, :concept_map=>concept_map})
       end
 
       extend self
