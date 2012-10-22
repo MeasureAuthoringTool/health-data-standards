a0ce8ea1c033daadb124f80bebd2c9b513fb6076
Rob Dingwell <bobd@mitre.org>
2012-10-22 15:21:03 -0400

    Updating valueset api to use proxy tickets and one time use tickets

--- lib/health-data-standards/util/vs_api.rb
+++ lib/health-data-standards/util/vs_api.rb
@@ -1,18 +1,22 @@
 require 'rest_client'
+require 'uri'
 module HealthDataStandards
   module Util
 		class VSApi			
-			attr_accessor :api_url, :ticket_url, :username, :password
+			attr_accessor :api_url, :ticket_url,  :username, :password
 
-			def initialize(ticket_url,api_url,username,password)
+			def initialize(ticket_url, api_url,username,password)
 				@api_url = api_url
 				@ticket_url = ticket_url
 				@username = username
 				@password = password
+        if !@ticket_url.end_with?("/")
+          @ticket_url = @ticket_url + "/"
+        end
 			end
 
 			def get_valueset(oid,&block)
-				vs = RestClient.get api_url, {:params=>{id: oid, ticket: ticket}}
+				vs = RestClient.get api_url, {:params=>{id: oid, ticket: get_ticket}}
 				yield oid,vs if block_given?
 				vs
 			end
@@ -24,15 +28,19 @@ module HealthDataStandards
 				end
 			end
 
-			def ticket
-			 	@ticket ||= get_ticket
+			def proxy_ticket
+			 	@proxy_ticket ||= get_proxy_ticket
 			end
 
-			def get_ticket
+			def get_proxy_ticket
 				# the content type is set and the body is a string becuase the NLM service does not support urlencoded content and
 				# throws an error on that contnet type
-				RestClient.post ticket_url, "username=#{username}&password=#{password}", content_type: "application/text"
+				 RestClient.post ticket_url, {username: username, password: password}
 			end
+			
+			def get_ticket
+			  RestClient.post "#{ticket_url}#{proxy_ticket}", {service: "http://umlsks.nlm.nih.gov"}
+		  end
 		end
   end
 end
--- test/unit/util/vs_api_test.rb
+++ test/unit/util/vs_api_test.rb
@@ -5,12 +5,13 @@ class EntryTest < MiniTest::Unit::TestCase
   
 	def test_api
 
-		stub_request(:post,'https://localhost/token').with(:body =>"username=myusername&password=mypassword").to_return( :body=>"ticket")
+		stub_request(:post,'https://localhost/token/').with(:body =>{"password"=>"mypassword", "username"=>"myusername"}).to_return( :body=>"proxy_ticket")
 		stub_request(:get,'https://localhost/vsservice').with(:query =>{:id=>"oid", :ticket=>"ticket"}).to_return( :body=>"<ValuesetResponse/>")
-
+    stub_request(:post,'https://localhost/token/proxy_ticket').with(:body =>{"service"=>"http://umlsks.nlm.nih.gov"}).to_return( :body=>"ticket")
     api = HealthDataStandards::Util::VSApi.new("https://localhost/token", "https://localhost/vsservice", "myusername", "mypassword")
 
-    assert_equal "ticket" , api.ticket
+    assert_equal "proxy_ticket" , api.proxy_ticket
+    assert_equal "ticket", api.get_ticket
     assert_equal "<ValuesetResponse/>", api.get_valueset("oid")
 
     api.get_valueset("oid") do |oid,vs|
