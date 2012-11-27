5359e1fef504ee5d24625453c194906fbc98e5f5
Andy Gregorowicz <andy@mitre.org>
2012-11-27 16:26:36 -0500

    tabs to spaces

--- lib/health-data-standards/models/svs/value_set.rb
+++ lib/health-data-standards/models/svs/value_set.rb
@@ -2,7 +2,7 @@ module HealthDataStandards
   module SVS
     class ValueSet
 
-    	include Mongoid::Document
+      include Mongoid::Document
       field :oid, type: String
       field :display_name, type: String
       field :version, type: String
@@ -11,19 +11,19 @@ module HealthDataStandards
       scope :by_oid, ->(oid){where(:oid => oid)}
 
       def self.load_from_xml(doc)
-      	  doc.root.add_namespace_definition("vs","urn:ihe:iti:svs:2008")
-      	 	vs_element = doc.at_xpath("/vs:RetrieveValueSetResponse/vs:ValueSet")
-      	 	if vs_element
-      	 		vs = ValueSet.new(oid: vs_element["ID"], display_name: vs_element["displayName"], version: vs_element["version"])
-      	 		concepts = vs_element.xpath("//vs:Concept").collect do |con|
-      	 				Concept.new(code: con["code"], 
-      	 					code_system_name: con["codeSystemName"], 
-      	 					code_system_version: con["code_system_version"],
-      	 				  display_name: con["displayName"],code_system: con["codeSystem"])
-      	 		   end
-                vs.concepts = concepts
-      	 		return vs
-      	 	end
+        doc.root.add_namespace_definition("vs","urn:ihe:iti:svs:2008")
+        vs_element = doc.at_xpath("/vs:RetrieveValueSetResponse/vs:ValueSet")
+        if vs_element
+          vs = ValueSet.new(oid: vs_element["ID"], display_name: vs_element["displayName"], version: vs_element["version"])
+          concepts = vs_element.xpath("//vs:Concept").collect do |con|
+            Concept.new(code: con["code"], 
+                        code_system_name: con["codeSystemName"], 
+                        code_system_version: con["code_system_version"],
+                        display_name: con["displayName"],code_system: con["codeSystem"])
+          end
+          vs.concepts = concepts
+          return vs
+        end
       end
     end
   end
