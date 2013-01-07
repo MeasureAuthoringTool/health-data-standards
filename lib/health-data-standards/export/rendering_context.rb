681df76b1dc69135c841c1008b1887eb82f8a867
Andy Gregorowicz <andy@mitre.org>
2013-01-07 15:04:33 -0500

    Adding some documentation

--- lib/health-data-standards/export/rendering_context.rb
+++ lib/health-data-standards/export/rendering_context.rb
@@ -1,7 +1,19 @@
-require 'ostruct'
-
 module HealthDataStandards
   module Export
+    # Used to actually render stuff. A RenderingContext needs to be set up with
+    # a template helper and may be provided with extensions.
+    #
+    # :call-seq:
+    #   template_helper = HealthDataStandards::Export::TemplateHelper.new('cat1', 'cat1')
+    #   rendering_context = HealthDataStandards::Export::RenderingContext.new
+    #   rendering_context.template_helper = template_helper
+    #   rendering_context.extensions = [HealthDataStandards::Export::Helper::Cat1ViewHelper]
+    #
+    # In this case, a RenderingContext is being set up to generate Category 1 files. It is
+    # passed a template helper to finds the correct ERb templates to render from. It is also
+    # given an extension. This is just a Ruby Module which will have its methods exposed to
+    # the templates. RenderingContext will assume that extensions is an Array and will include
+    # multiple extensions if more than one is provided.  
     class RenderingContext < OpenStruct
       include ViewHelper
       attr_accessor :template_helper, :extensions
@@ -28,7 +40,7 @@ module HealthDataStandards
             rendering_context.extend(extension)
           end
         end
-        eruby = Erubis::EscapedEruby.new(erb)
+        eruby = Erubis::EscapedEruby.new(erb) # TODO: cache these
         eruby.result(rendering_context.my_binding)
       end
     end
--- lib/health-data-standards/export/template_helper.rb
+++ lib/health-data-standards/export/template_helper.rb
@@ -1,5 +1,14 @@
 module HealthDataStandards
   module Export
+    # Class that finds ERb templates. Here is how it can be configured:
+    # [template_format] What format (C32, CCDA, etc) are we looking for. This will cause
+    #                   the TemplateHelper to look for template_name.template_format.erb
+    # [template_subdir] The sub directory where templates live. If none is provided, it
+    #                   will look for templates in the root of the template_directory
+    # [template_directory] The root directory to look in for templates. By default, it
+    #                      is in the template folder of this gem. It can be handy to
+    #                      provide a different directory if you want to use this class
+    #                      outside of the HDS gem
     class TemplateHelper
       def initialize(template_format, template_subdir = nil, template_directory = nil)
         @template_format = template_format
@@ -17,10 +26,14 @@ module HealthDataStandards
         end
       end
 
+      # Returns the raw ERb for the template_name provided. This method will look in
+      # template_directory/template_subdir/template_name.template_format.erb
       def template(template_name)
         File.read(File.join(template_root, "#{template_name}.#{@template_format}.erb"))
       end
 
+      # Basically the same template, but prepends an underscore to the template name
+      # to mimic the Rails convention for template fragments
       def partial(partial_name)
         template("_#{partial_name}")
       end
