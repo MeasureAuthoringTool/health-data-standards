5b73c104db99b5a6c0a7a89b658c7e0a2dd423b5
ahubley <ahubley@mitre.org>
2013-01-08 15:35:58 -0500

    added support for rollover code descriptions in html export

--- lib/health-data-standards/export/html.rb
+++ lib/health-data-standards/export/html.rb
@@ -3,10 +3,31 @@ module HealthDataStandards
     module HTML
       include TemplateHelper
 
-      def export(patient, concept_map=nil)
+      def export(patient, concept_map=nil, code_map=nil)
         self.template_format = "html"
         self.template_subdir = "html"
-        render(:template => 'show', :locals => {:patient => patient, :concept_map=>concept_map})
+
+        self.generate_code_map
+
+        render(:template => 'show', :locals => {:patient => patient, :concept_map=>concept_map, :code_map => @code_map})
+
+      end
+      
+      def generate_code_map
+        @code_map ||= self.build_code_map
+      end
+
+      def build_code_map
+        super_code_map = {}
+        val_set_array = HealthDataStandards::SVS::ValueSet.all.to_a
+        val_set_array.each do |valset| 
+          valset.concepts.each do |concept|
+            code_group_name = HealthDataStandards::SVS::ValueSet.normalize_code_set_name(concept.code_system_name)
+            super_code_map[code_group_name] ||= {}
+            super_code_map[code_group_name][concept.code] = concept.display_name
+          end
+        end
+        super_code_map
       end
 
       extend self
