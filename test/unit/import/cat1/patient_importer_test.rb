bb1cada42738a67280fc4d24916a65910105dd86
Andy Gregorowicz <andy@mitre.org>
2013-01-23 11:36:28 -0500

    Implemented importing of discharge medications

--- test/unit/import/cat1/patient_importer_test.rb
+++ test/unit/import/cat1/patient_importer_test.rb
@@ -2,12 +2,26 @@ require 'test_helper'
 
 class PatientImporterTest < MiniTest::Unit::TestCase
   def test_ecog_status_importing
-    doc = Nokogiri::XML(File.new('test/fixtures/cat1_fragments/ecog_fragment.xml'))
+    patient = build_record_from_xml('test/fixtures/cat1_fragments/ecog_fragment.xml')
+    ecog_status = patient.conditions.first
+    assert ecog_status.codes['SNOMED-CT'].include?('423237006')
+  end
+
+  def test_discharge_medication_importing
+    patient = build_record_from_xml('test/fixtures/cat1_fragments/discharge_medication_fragment.xml')
+    discharge = patient.medications.first
+    discharge.codes['RxNorm'].include?('994435')
+  end
+
+  private
+
+  def build_record_from_xml(xml_file)
+    doc = Nokogiri::XML(File.new(xml_file))
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     
     patient = Record.new
     HealthDataStandards::Import::Cat1::PatientImporter.instance.import_sections(patient, doc)
-    ecog_status = patient.conditions.first
-    assert ecog_status.codes['SNOMED-CT'].include?('423237006')
+    patient
   end
+
 end
