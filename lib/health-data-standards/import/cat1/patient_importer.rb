44c7394092226a42e6c2b2c49c72f0bb75d575bf
Michael D. Norman <mnorman@mitre.org>
2013-01-29 11:33:43 -0500

    Procedure Order Import Changes

--- lib/health-data-standards/import/cat1/patient_importer.rb
+++ lib/health-data-standards/import/cat1/patient_importer.rb
@@ -26,7 +26,7 @@ module HealthDataStandards
           @section_importers[:procedures] << CDA::ProcedureImporter.new(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.59']"))
           @section_importers[:allergies] = []
           @section_importers[:allergies] << ProcedureIntoleranceImporter.new()
-
+          @section_importers[:procedures] << ProcedureOrderImporter.new()
         end
 
         def parse_cat1(doc)
---
+++ lib/health-data-standards/import/cat1/procedure_order_importer.rb
@@ -0,0 +1,20 @@
+module HealthDataStandards
+  module Import
+    module Cat1
+      class ProcedureOrderImporter < CDA::ProcedureImporter
+
+        def initialize
+          super(CDA::EntryFinder.new("//cda:procedure[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.63']"))
+          @entry_class = Procedure
+        end
+
+        def extract_dates(parent_element, entry, element_name="author")
+          if parent_element.at_xpath("cda:#{element_name}/cda:time/@value")
+            entry.start_time = HL7Helper.timestamp_to_integer(parent_element.at_xpath("cda:#{element_name}/cda:time")['value'])
+          end
+        end
+
+      end
+    end
+  end
+end
