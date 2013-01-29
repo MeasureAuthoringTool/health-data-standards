b305a2b6b4e5f502ccd83b1b9e8262ce4290aa83
Michael D. Norman <mnorman@mitre.org>
2013-01-29 14:39:21 -0500

    Updated procedure_order_importer.rb to include status_code "ordered"

--- lib/health-data-standards/import/cat1/procedure_order_importer.rb
+++ lib/health-data-standards/import/cat1/procedure_order_importer.rb
@@ -1,12 +1,22 @@
 module HealthDataStandards
   module Import
     module Cat1
-      class ProcedureOrderImporter < CDA::ProcedureImporter
-
+      class ProcedureOrderImporter < CDA::SectionImporter #< CDA::ProcedureImporter
         def initialize
           super(CDA::EntryFinder.new("//cda:procedure[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.63']"))
           @entry_class = Procedure
         end
+        
+        def create_entry(entry_element, nrh = CDA::NarrativeReferenceHandler.new)
+          procedure = super
+          procedure.status_code = {'HL7 ActStatus' => ['ordered']}
+          extract_performer(entry_element, procedure)
+          extract_site(entry_element, procedure)
+          extract_negation(entry_element, procedure)
+          procedure
+        end
+
+        private
 
         def extract_dates(parent_element, entry, element_name="author")
           if parent_element.at_xpath("cda:#{element_name}/cda:time/@value")
@@ -14,6 +24,14 @@ module HealthDataStandards
           end
         end
 
+        def extract_performer(parent_element, procedure)
+          performer_element = parent_element.at_xpath("./cda:performer")
+          procedure.performer = import_actor(performer_element) if performer_element
+        end
+
+        def extract_site(parent_element, procedure)
+          procedure.site = extract_code(parent_element, "./cda:targetSiteCode")
+        end
       end
     end
   end
