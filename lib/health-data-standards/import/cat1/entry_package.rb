9d8ac81e2931f0ad6c0eac89d0b929820e6ccfd3
Michael D. Norman <mnorman@mitre.org>
2013-02-15 10:11:58 -0500

    Stylistic changes

--- lib/health-data-standards/import/cat1/entry_package.rb
+++ lib/health-data-standards/import/cat1/entry_package.rb
@@ -13,10 +13,12 @@ module HealthDataStandards
 
         def package_entries (doc, nrh)
           entries = self.importer_type.create_entries(doc, nrh)
-          entries.map {|entry| entry.oid = self.hqmf_oid, entry.status = self.status}
+          entries.each do |entry| 
+            entry.oid = self.hqmf_oid
+            entry.status = self.status
+          end          
           entries
         end
-        
       end
     end
   end
