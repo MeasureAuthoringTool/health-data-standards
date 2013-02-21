8dc4f63f52ba7dc82ee559e39338b4e5e72788f1
Rob Dingwell <bobd@mitre.org>
2013-02-21 17:44:00 -0500

    testing railtie locally

--- lib/health-data-standards/railtie.rb
+++ lib/health-data-standards/railtie.rb
@@ -3,5 +3,10 @@ module HealthDataStandards
     initializer 'Rails logger' do
       HealthDataStandards.logger = Rails.logger
     end
+    puts "Railtie"
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+    end
+
   end
 end
