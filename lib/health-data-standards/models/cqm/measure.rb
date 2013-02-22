52bbf6d35f0f6237c1bbe5847c06cc31e3ed9e87
Rob Dingwell <bobd@mitre.org>
2013-02-22 10:38:27 -0500

    removing measure methods.  These should be application specific

--- lib/health-data-standards/models/cqm/measure.rb
+++ lib/health-data-standards/models/cqm/measure.rb
@@ -30,63 +30,6 @@ module HealthDataStandards
 		  validates_presence_of :name
 		  
 
-		  def key
-		    "#{self['id']}#{sub_id}"
-		  end
-		  
-		  def is_cv?
-		    ! population_ids[QME::QualityReport::MSRPOPL].nil?
-		  end
-
-		  def self.installed
-		    Measure.order_by([["id", :asc],["sub_id", :asc]]).to_a
-		  end
-		  
-
-		  # Finds all measures and groups the sub measures
-		  # @return Array - This returns an Array of Hashes. Each Hash will represent a top level measure with an ID, name, and category.
-		  #                 It will also have an array called subs containing hashes with an ID and name for each sub-measure.
-		  def self.all_by_measure
-		    reduce = 'function(obj,prev) {
-		                if (obj.sub_id != null)
-		                  prev.subs.push({id : obj.id + obj.sub_id, name : obj.subtitle});
-		              }'
-		    
-		    MONGO_DB.command( :group=> {:ns=>"measures", :key => {:id=>1, :name=>1, :category=>1}, :initial => {:subs => []}, "$reduce" => reduce})["retval"]
-		  end
-
-		  def display_name
-		    "#{self['nqf_id']} - #{name}"
-		  end
-		  
-
-		  def set_id
-		    self.hqmf_set_id
-		  end
-
-		  def measure_id
-		    self['id']
-		  end
-
-		  def continuous?
-		    population_ids[QME::QualityReport::MSRPOPL]
-		  end
-
-		  def title
-		    self.name
-		  end
-
-		  def all_data_criteria
-		    return @crit if @crit
-		    @crit = []
-		    self.data_criteria.each do |dc|
-		      dc.each_pair do |k,v|
-		        @crit <<HQMF::DataCriteria.from_json(k,v)
-		      end
-		    end
-		    @crit
-		  end
-
 
 		end
 	end
