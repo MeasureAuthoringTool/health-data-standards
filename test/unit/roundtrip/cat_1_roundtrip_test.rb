c68bd3f178f38d8f20a73343b1c625a943449ef1
Michael D. Norman <mnorman@mitre.org>
2013-03-22 10:05:58 -0400

    Cat1 Roundtrip Test patient Mary Berry JSON created, Cat1RoundtripTest modified to leverage

--- test/unit/roundtrip/cat_1_roundtrip_test.rb
+++ test/unit/roundtrip/cat_1_roundtrip_test.rb
@@ -7,20 +7,19 @@ class Cat1RoundtripTest < MiniTest::Unit::TestCase
   # and imported.
 
   def test_round_trip
-    #Export - TODO: Need to save maxine as JSON or something outside of my cypress DB so she can be instantiated by everyone running test suite - OR ADD her encounter to Barry and use him
-    @maxine = Record.with(database: "cypress_development").where({first: "Maxine"}).first
-    @patient = @maxine
+    # Export
+    @mary = Record.where({first: "Mary"}).first
+    @patient = @mary
     @start_date = Time.now.years_ago(1)
     @end_date = Time.now
     @measures = MEASURES
     @qrda_xml = HealthDataStandards::Export::Cat1.new.export(@patient, @measures, @start_date, @end_date)
     @doc = Nokogiri::XML(@qrda_xml)
     @doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    File.open('maxine-nqf0002-fixture.xml', 'w') { |file| file.write(@doc) }
+    File.open('mary.xml', 'w') { |file| file.write(@doc) }
 
-    # #TODO-NEXT need to go through the entries in the record object and compare content from exported record to imported record using assertion statements
-    # #DOn't compare on the record level because the order of elements could screw it up as well as the fact that they will probably have different mongoid IDs
-    doc = Nokogiri::XML(File.new('maxine-nqf0002-fixture.xml'))
+    # Import
+    doc = Nokogiri::XML(File.new('mary.xml'))
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
     @patient_import = Record.new
