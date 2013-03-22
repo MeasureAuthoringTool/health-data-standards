ef5d402e1595b8735ed157dafd059d185a3f335b
Michael D. Norman <mnorman@mitre.org>
2013-03-22 14:16:12 -0400

    Partial Cleanup of Cat1RoundtripTest

--- test/unit/roundtrip/cat_1_roundtrip_test.rb
+++ test/unit/roundtrip/cat_1_roundtrip_test.rb
@@ -8,32 +8,35 @@ class Cat1RoundtripTest < MiniTest::Unit::TestCase
 
   def test_round_trip
     # Export
-    @mary = Record.where({first: "Mary"}).first
-    @patient = @mary
-    @start_date = Time.now.years_ago(1)
-    @end_date = Time.now
-    @measures = MEASURES
-    @qrda_xml = HealthDataStandards::Export::Cat1.new.export(@patient, @measures, @start_date, @end_date)
-    @doc = Nokogiri::XML(@qrda_xml)
-    @doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    File.open('mary.xml', 'w') { |file| file.write(@doc) }
-
-    # Import
-    doc = Nokogiri::XML(File.new('mary.xml'))
+    mary = Record.where({first: "Mary"}).first
+    patient = mary
+    start_date = Time.now.years_ago(1)
+    end_date = Time.now
+    measures = MEASURES
+    qrda_xml = HealthDataStandards::Export::Cat1.new.export(patient, measures, start_date, end_date)
+    doc = Nokogiri::XML(qrda_xml)
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
-    doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
-    @patient_import = Record.new
-    HealthDataStandards::Import::Cat1::PatientImporter.instance.import_sections(@patient_import, doc)
-    encounter = @patient_import.encounters.first
-    expected_start = HealthDataStandards::Util::HL7Helper.timestamp_to_integer('20120430120000')
-    expected_end = HealthDataStandards::Util::HL7Helper.timestamp_to_integer('20120430130000')
-    assert encounter.codes['CPT'].include?('99201')
-    assert_equal expected_start, encounter.start_time
-    assert_equal expected_end, encounter.end_time
+    File.open('mary.xml', 'w') { |file| file.write(doc) }
+    #mary_xml = StringIO.new(doc)
+    #mary_xml.rewind
 
-    #Compare Encounters
-    assert_equal @patient.encounters, @patient_import.encounters
+    # Import 
+    #doc_import = Nokogiri::XML(mary_xml.read) #TODO - transition from File IO to StringIO
+    doc_import = Nokogiri::XML(File.new('mary.xml'))  
+    doc_import.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    doc_import.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
+    patient_import = Record.new
+    HealthDataStandards::Import::Cat1::PatientImporter.instance.import_sections(patient_import, doc_import)
+    encounter = patient.encounters.first
+    encounter_import = patient_import.encounters.first
+
+    #Compare Encounter Attributes
+    assert_equal encounter.codes, encounter_import.codes
+    assert_equal encounter.start_time, encounter_import.start_time
+    assert_equal encounter.end_time, encounter_import.end_time
+    
+    #Compare Entire Encounters Section
+    assert_equal patient.encounters, patient_import.encounters
 
   end
-
 end
