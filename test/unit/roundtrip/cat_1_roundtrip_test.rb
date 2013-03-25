3293f0d818abcf932591e4195935308644fba452
Michael D. Norman, Ph.D <mnorman@mitre.org>
2013-03-25 12:48:15 -0400

    Cat 1 Roundtriptest using stringio and file output

--- test/unit/roundtrip/cat_1_roundtrip_test.rb
+++ test/unit/roundtrip/cat_1_roundtrip_test.rb
@@ -17,11 +17,8 @@ class Cat1RoundtripTest < MiniTest::Unit::TestCase
     doc = Nokogiri::XML(qrda_xml)
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     File.open('mary.xml', 'w') { |file| file.write(doc) }
-    #mary_xml = StringIO.new(doc)
-    #mary_xml.rewind
 
     # Import 
-    #doc_import = Nokogiri::XML(mary_xml.read) #TODO - transition from File IO to StringIO
     doc_import = Nokogiri::XML(File.new('mary.xml'))  
     doc_import.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     doc_import.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
@@ -39,4 +36,38 @@ class Cat1RoundtripTest < MiniTest::Unit::TestCase
     assert_equal patient.encounters, patient_import.encounters
 
   end
+
+
+  def test_round_trip_stringio
+    # Export
+    mary = Record.where({first: "Mary"}).first
+    patient = mary
+    start_date = Time.now.years_ago(1)
+    end_date = Time.now
+    measures = MEASURES
+    qrda_xml = HealthDataStandards::Export::Cat1.new.export(patient, measures, start_date, end_date)
+    doc = Nokogiri::XML(qrda_xml)
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')    
+    mary_xml = StringIO.new(doc.to_s)
+
+    # Import 
+    doc_import = Nokogiri::XML(mary_xml.string)
+    doc_import.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    doc_import.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
+    patient_import = Record.new
+    HealthDataStandards::Import::Cat1::PatientImporter.instance.import_sections(patient_import, doc_import)
+    encounter = patient.encounters.first
+    encounter_import = patient_import.encounters.first
+
+    #Compare Encounter Attributes
+    assert_equal encounter.codes, encounter_import.codes
+    assert_equal encounter.start_time, encounter_import.start_time
+    assert_equal encounter.end_time, encounter_import.end_time
+    
+    #Compare Entire Encounters Section
+    assert_equal patient.encounters, patient_import.encounters
+
+  end
+
+
 end
