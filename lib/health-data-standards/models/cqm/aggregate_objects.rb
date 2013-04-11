946c5798010d4f6474e0ec4ed7d7468e1842e879
Andy Gregorowicz <andy@mitre.org>
2013-04-11 09:06:44 -0400

    Implementing the export of stratifications

--- lib/health-data-standards/models/cqm/aggregate_objects.rb
+++ lib/health-data-standards/models/cqm/aggregate_objects.rb
@@ -1,11 +1,35 @@
 module HealthDataStandards
   module CQM
+
+    module PopulationSelectors
+      def numerator
+        populations.find {|pop| pop.type = 'NUMER'}
+      end
+
+      def denominator
+        populations.find {|pop| pop.type = 'DENOM'}
+      end
+
+      def denominator_exceptions
+        populations.find {|pop| pop.type = 'DENEXCEP'}
+      end
+
+      def denominator_exclusions
+        populations.find {|pop| pop.type = 'DENEX'}
+      end
+
+      def population_count(population_type)
+        populations.find {|pop| pop.type = population_type}.value
+      end
+    end
+
     class Population
       attr_accessor :type, :value, :id
     end
 
     class Stratification
       attr_accessor :id, :populations
+      include PopulationSelectors
 
       def initialize
         @populations = []
@@ -14,6 +38,8 @@ module HealthDataStandards
 
     class AggregateCount
       attr_accessor :measure_id, :stratifications, :top_level_populations
+      alias :populations :top_level_populations
+      include PopulationSelectors
 
       def initialize
         @stratifications = []
@@ -26,26 +52,6 @@ module HealthDataStandards
 
       def performance_rate
         numerator.value.to_f / (denominator.value - denominator_exclusions.value - denominator_exceptions.value)
-      end
-
-      def numerator
-        top_level_populations.find {|pop| pop.type = 'NUMER'}
-      end
-
-      def denominator
-        top_level_populations.find {|pop| pop.type = 'DENOM'}
-      end
-
-      def denominator_exceptions
-        top_level_populations.find {|pop| pop.type = 'DENEXCEP'}
-      end
-
-      def denominator_exclusions
-        top_level_populations.find {|pop| pop.type = 'DENEX'}
-      end
-
-      def population_count(population_type)
-        top_level_populations.find {|pop| pop.type = population_type}.value
       end
     end
   end
