a19d5d354fbb9592f451c249ec2d42030e1266cf
Liam Morley <liam@carpeliam.com>
2013-08-07 21:18:07 -0400

    pass data_criteria into Cat 1 templates

--- lib/health-data-standards/export/helper/cat1_view_helper.rb
+++ lib/health-data-standards/export/helper/cat1_view_helper.rb
@@ -4,13 +4,13 @@ module HealthDataStandards
       module Cat1ViewHelper
         include HealthDataStandards::Export::Helper::ScoopedViewHelper
 
-        def render_data_criteria(dc_oid, vs_oid, entries)
+        def render_data_criteria(dc, entries)
           html_array = entries.map do |entry|
               bundle_id = entry.record ? entry.record["bundle_id"] : nil
-              vs_map = (value_set_map(bundle_id) || {})[vs_oid]
-              binding.pry if vs_map.nil? || vs_map.empty?
-              render(:partial => HealthDataStandards::Export::QRDA::EntryTemplateResolver.partial_for(dc_oid, vs_oid), :locals => {:entry => entry,
-                                                                                                                                   :value_set_oid => vs_oid,
+              vs_map = (value_set_map(bundle_id) || {})[dc['value_set_oid']]
+              render(:partial => HealthDataStandards::Export::QRDA::EntryTemplateResolver.partial_for(dc['data_criteria_oid'], dc['value_set_oid']), :locals => {:entry => entry,
+                                                                                                                                   :data_criteria => dc['data_criteria'],
+                                                                                                                                   :value_set_oid => dc['value_set_oid'],
                                                                                                                                    :value_set_map => vs_map})
           end
           html_array.join("\n")
@@ -21,7 +21,7 @@ module HealthDataStandards
           udcs = unique_data_criteria(measures)
           data_criteria_html = udcs.map do |udc|
             entries = entries_for_data_criteria(udc['data_criteria'], patient)
-            render_data_criteria(udc['data_criteria_oid'], udc['value_set_oid'], entries)          
+            render_data_criteria(udc, entries)          
           end
           data_criteria_html.compact.join("\n")
         end
