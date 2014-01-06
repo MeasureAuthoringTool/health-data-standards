239fc3e2fbb270812edeaea4cba12cabfb51e024
Andrew Hubley <ahubley@mitre.org>
2014-01-06 15:22:29 -0500

    cat1 encounter importer/export transfer from/to support

---
+++ test/unit/import/cat1/encounter_transfer_importer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EncounterTransferImporterTest < MiniTest::Unit::TestCase
+  def test_encounter_transfer_importing
+    doc = Nokogiri::XML(File.new('test/fixtures/cat1_fragments/encounter_performed_fragment.xml'))
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    nrh = HealthDataStandards::Import::CDA::NarrativeReferenceHandler.new
+    nrh.build_id_map(doc)
+    enc = HealthDataStandards::Import::Cat1::EntryPackage.new(HealthDataStandards::Import::CDA::EncounterImporter.new(HealthDataStandards::Import::CDA::EntryFinder.new("//cda:encounter[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.23']")), '2.16.840.1.113883.3.560.1.79', 'performed')
+    encounters = enc.package_entries(doc, nrh)
+    encounter = encounters[0]
+    from_time = 20120214
+    to_time = 20120214
+    assert encounter.transferFrom.codes['HL7 Healthcare Service Location'].include?("1024-9")
+    assert encounter.transferTo.codes['HL7 Healthcare Service Location'].include?("1194-0")
+    assert_equal encounter.transferFrom.time, from_time
+    assert_equal encounter.transferTo.time, to_time
+  end
+end
