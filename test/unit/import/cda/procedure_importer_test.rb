6c7fa134594d8418ed2af9f640d0d684c92be930
Mark Yoon <yoon@northwestern.edu>
2014-01-15 15:43:52 -0600

    importing scalars

---
+++ test/unit/import/cda/procedure_importer_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ProcedureImporterTest < MiniTest::Unit::TestCase
+  def setup
+    @si = HealthDataStandards::Import::CDA::ProcedureImporter.new(HealthDataStandards::Import::CDA::EntryFinder.new('/cda:simple/cda:entry'))
+    @doc = Nokogiri::XML(File.new('test/fixtures/section_importer.xml'))
+    @doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+  end
+
+  def test_extracting_scalars
+    entries = @si.create_entries(@doc)
+    entry = entries[3]
+    pq_value = PhysicalQuantityResultValue.new(scalar: "eleventeen", units: nil)
+    assert_equal pq_value.scalar, entry.values[0].scalar
+    assert_equal pq_value.units, entry.values[0].units
+  end
+end
