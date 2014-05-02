70dd38ef6b32faaa0b3311caa26a785d57c7d72a
Andy Gregorowicz <andy@mitre.org>
2014-05-02 10:32:45 -0400

    Switching from passing in a File to a Dir
    It appears that using JRuby File.new(a_directory) throws a Permission denied
    exception. This code works fine with File.new on MRI/YARV.

--- test/unit/import/bulk_record_importer_test.rb
+++ test/unit/import/bulk_record_importer_test.rb
@@ -17,7 +17,7 @@ class BulkRecordImporterTest < MiniTest::Unit::TestCase
 
 
   def test_bulk_record_import_xml_directory
-    file = File.new('test/fixtures/bulk_importer/bulk_import_sample')
+    file = Dir.new('test/fixtures/bulk_importer/bulk_import_sample')
     HealthDataStandards::Import::BulkRecordImporter.import_directory(file,"./tmp/failed")
     patient1 = Record.where({"first" => "Cancer_Adult_Female"}).first
     patient2 = Record.where({"first" => "Cancer_Adult_Male"}).first
@@ -26,7 +26,7 @@ class BulkRecordImporterTest < MiniTest::Unit::TestCase
   end
 
   def test_bulk_record_import_json_directory
-    file = File.new('test/fixtures/bulk_importer/json')
+    file = Dir.new('test/fixtures/bulk_importer/json')
     HealthDataStandards::Import::BulkRecordImporter.import_directory(file,"./tmp/failed")
     patient1 = Record.where({"first" => "Cancer_Adult_Female"}).first
     patient2 = Record.where({"first" => "Cancer_Adult_Male"}).first
