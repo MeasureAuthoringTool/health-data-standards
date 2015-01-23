8c1b5a8b871c928797996f6a47ce8d3b2162aae5
Janoo Fernandes <smoothclip@gmail.com>
2015-01-23 14:25:25 -0500

    Resolve template id mismatch and fix data criteria ids to resolve JS parsing issues

--- lib/hqmf-parser/2.0/types.rb
+++ lib/hqmf-parser/2.0/types.rb
@@ -3,23 +3,23 @@ module HQMF2
   # don't specify any restrictions on that value
   class AnyValue
     attr_reader :type
-    
+
     def initialize(type='ANYNonNull')
-      @type = type 
+      @type = type
     end
-    
+
     def to_model
       HQMF::AnyValue.new(@type)
-    end    
+    end
   end
-  
+
   # Represents a bound within a HQMF pauseQuantity, has a value, a unit and an
   # inclusive/exclusive indicator
   class Value
     include HQMF2::Utilities
-    
+
     attr_reader :type, :unit, :value
-    
+
     def initialize(entry, default_type='PQ', force_inclusive=false)
       @entry = entry
       @type = attr_val('./@xsi:type') || default_type
@@ -27,11 +27,11 @@ module HQMF2
       @value = attr_val('./@value')
       @force_inclusive = force_inclusive
     end
-    
+
     def inclusive?
       attr_val("../@#{@entry.name}Closed") == 'true' || @force_inclusive
     end
-    
+
     def derived?
       case attr_val('./@nullFlavor')
       when 'DER'
@@ -40,7 +40,7 @@ module HQMF2
         false
       end
     end
-    
+
     def expression
       if !derived?
         nil
@@ -48,17 +48,17 @@ module HQMF2
         attr_val('./cda:expression/@value')
       end
     end
-    
+
     def to_model
       HQMF::Value.new(type,unit,value,inclusive?,derived?,expression)
     end
   end
-  
+
   # Represents a HQMF physical quantity which can have low and high bounds
   class Range
     include HQMF2::Utilities
     attr_accessor :low, :high, :width
-    
+
     def initialize(entry, type=nil)
       @type = type
       @entry = entry
@@ -68,13 +68,14 @@ module HQMF2
         @high = optional_value("#{default_element_name}/cda:high", default_bounds_type)
         @high = nil unless @high.try(:value)
         @width = optional_value("#{default_element_name}/cda:width", 'PQ')
+        detect_period
       end
     end
-    
+
     def type
       @type || attr_val('./@xsi:type')
     end
-    
+
     def to_model
       lm = low ? low.to_model : nil
       hm = high ? high.to_model : nil
@@ -85,9 +86,9 @@ module HQMF2
       end
       HQMF::Range.new(model_type, lm, hm, wm)
     end
-    
+
     private
-    
+
     def optional_value(xpath, type)
       value_def = @entry.at_xpath(xpath, HQMF2::Document::NAMESPACES)
       if value_def
@@ -107,7 +108,7 @@ module HQMF2
         'cda:uncertainRange'
       end
     end
-    
+
     def default_bounds_type
       case type
       when 'IVL_TS'
@@ -116,39 +117,52 @@ module HQMF2
         'PQ'
       end
     end
+
+    # TODO: Update this to actually compute the correct end time for the period
+    def detect_period
+      if @low && @high.nil?
+        period = optional_value("cda:period", default_bounds_type)
+        if period.try(:unit) == 'a' && period.try(:value) == '1'
+          high_entry = @entry.at_xpath("#{default_element_name}/cda:low", HQMF2::Document::NAMESPACES).dup
+          high_entry.attributes["value"].value = '20151231' if @low.value == '20150101'
+          @high = Value.new(high_entry, default_bounds_type, attr_val("cda:phase/@highClosed") == 'true')
+          @high = nil unless @high.try(:value)
+        end
+      end
+    end
   end
-  
+
   # Represents a HQMF effective time which is a specialization of a interval
   class EffectiveTime < Range
     def initialize(entry)
       super
     end
-    
+
     def type
       'IVL_TS'
     end
   end
-  
+
   # Represents a HQMF CD value which has a code and codeSystem
   class Coded
     include HQMF2::Utilities
-    
+
     def initialize(entry)
       @entry = entry
     end
-    
+
     def type
       attr_val('./@xsi:type') || 'CD'
     end
-    
+
     def system
       attr_val('./@codeSystem')
     end
-    
+
     def code
       attr_val('./@code')
     end
-    
+
     def code_list_id
       attr_val('./@valueSet')
     end
@@ -168,13 +182,13 @@ module HQMF2
     def unit
       nil
     end
-    
+
     def to_model
       HQMF::Coded.new(type, system, code, code_list_id, title)
     end
-    
+
   end
-  
+
   class SubsetOperator
     include HQMF2::Utilities
 
@@ -227,10 +241,10 @@ module HQMF2
       HQMF::SubsetOperator.new(type, vm)
     end
   end
-  
+
   class TemporalReference
     include HQMF2::Utilities
-    
+
     attr_reader :type, :reference, :range
 
     def initialize(entry)
@@ -242,18 +256,18 @@ module HQMF2
         @range = HQMF2::Range.new(range_def, 'IVL_PQ')
       end
     end
-    
+
     def to_model
       rm = range ? range.to_model : nil
       HQMF::TemporalReference.new(type, reference.to_model, rm)
-    end  
+    end
   end
 
 # Represents a HQMF reference to a data criteria that has a given type
   class TypedReference
     include HQMF2::Utilities
     attr_accessor :id
-    
+
     # Create a new HQMF::Reference
     # @param [String] id
     def initialize(entry)
@@ -261,7 +275,7 @@ module HQMF2
     end
 
     def reference
-      attr_val('./cda:id/@extension')
+      strip_tokens attr_val('./cda:id/@extension')
     end
 
     def type
@@ -271,28 +285,31 @@ module HQMF2
     def mood
        attr_val('./@moodCode')
     end
-    
+
     def to_model
       HQMF::TypedReference.new(reference,type,mood)
-    end  
-    
+    end
+
   end
 
   # Represents a HQMF reference from a precondition to a data criteria
   class Reference
     include HQMF2::Utilities
-    
+
     def initialize(entry)
       @entry = entry
     end
-    
+
     def id
-      attr_val('./@extension')
+      id = attr_val('./@extension')
+      # Handle MeasurePeriod references for calculation code
+      id = 'MeasurePeriod' if id == 'measureperiod'
+      strip_tokens id
     end
-    
+
     def to_model
       HQMF::Reference.new(id)
     end
   end
-  
+
 end
