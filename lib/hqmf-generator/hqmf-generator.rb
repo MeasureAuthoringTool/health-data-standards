3a09a741d20769a4b24dab0b14ab7635c3a208c1
Rob Dingwell <bobd@mitre.org>
2015-03-05 18:26:40 -0500

    fixing issue that caused extraneous population groups to be created

--- lib/hqmf-generator/hqmf-generator.rb
+++ lib/hqmf-generator/hqmf-generator.rb
@@ -39,7 +39,7 @@ module HQMF2
       def xml_for_local_variable(criteria)
         name = @local_var_names[criteria.id]
         unless name
-          if criteria.specific_occurrence
+          if criteria.specific_occurrence && !criteria.id.starts_with?("Occurrence#{criteria.specific_occurrence}")
             name = "Occurrence#{criteria.specific_occurrence}#{criteria.id}"
           else
             name = criteria.id
