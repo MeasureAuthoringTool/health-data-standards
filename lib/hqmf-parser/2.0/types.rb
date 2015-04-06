85582057c1e5fe07a1d8c22892304cdf9337ea2b
Janoo Fernandes <smoothclip@gmail.com>
2015-04-06 15:07:45 -0400

    Align value parsing to utilize IG table for determining inclusivity

--- lib/hqmf-parser/2.0/types.rb
+++ lib/hqmf-parser/2.0/types.rb
@@ -32,8 +32,20 @@ module HQMF2
     end
 
     def inclusive?
-      v = attr_val("../@#{@entry.name}Closed")
-      v == nil || v != 'false' || @force_inclusive
+      # FIXME: NINF is used instead of 0 sometimes...? (not in the IG)
+      # FIXME: Given nullFlavor, but IG uses it and nullValue everywhere...
+      # Not always given highClosed=false for '<', so we can only default to '<='
+      less_than_equal_or_equiv = attr_val("../@lowClosed")!='false' &&
+        attr_val("../@highClosed")!='false' &&
+        ( attr_val("../cda:low/@value")=="0" ||
+          attr_val("../cda:low/@nullFlavor")=="NINF" ||
+          attr_val("../cda:low/@value")==attr_val("../cda:high/@value"))
+      greater_than_equal = attr_val("../@lowClosed")!='false' &&
+        attr_val("../@highClosed")=='false' &&
+        attr_val("../cda:low/@value")!="0" &&
+        attr_val("../cda:high/@nullFlavor")=="PINF"
+
+      less_than_equal_or_equiv || greater_than_equal || @force_inclusive
     end
 
     def derived?
