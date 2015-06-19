ca9393c9f06a09038e3aea9253fa7e4ed3f9b62f
Michael O'Keefe <mokeefe@mitre.org>
2015-06-19 15:33:29 -0400

    Updated VS API to include a VSApiV2 for the V2 API, and keep the V1 API the same

--- lib/health-data-standards/util/vs_api.rb
+++ lib/health-data-standards/util/vs_api.rb
@@ -2,10 +2,10 @@ require 'rest_client'
 require 'uri'
 module HealthDataStandards
   module Util
-  		class VSNotFoundError < StandardError
-  		end
+  	class VSNotFoundError < StandardError
+  	end
 
-		class VSApi			
+		class VSApi
 			attr_accessor :api_url, :ticket_url, :username, :password
 
 			def initialize(ticket_url, api_url, username, password)
@@ -15,7 +15,45 @@ module HealthDataStandards
 				@password = password
 			end
 
-			def get_valueset(oid, version=nil, include_draft=nil, &block)
+      def get_valueset(oid, effective_date=nil, include_draft=false, &block)
+				params = {id: oid, ticket: get_ticket}
+				params[:effectiveDate] = effective_date if effective_date
+				params[:includeDraft] = 'yes' if include_draft
+				vs = RestClient.get api_url, {:params=>params}
+				yield oid,vs if block_given?
+				vs
+			end
+
+			def process_valuesets(oids, effective_date=nil, &block)
+				oids.each do |oid|
+		    	vs = get_valueset(oid,effective_date)
+		    	yield oid,vs
+				end
+			end
+
+			def proxy_ticket
+			  @proxy_ticket ||= get_proxy_ticket
+			end
+
+			def get_proxy_ticket
+				# the content type is set and the body is a string becuase the NLM service does not support urlencoded content and
+				# throws an error on that contnet type
+				RestClient.post ticket_url, {username: username, password: password}
+			end
+
+			def get_ticket
+			  RestClient.post "#{ticket_url}/#{proxy_ticket}", {service: "http://umlsks.nlm.nih.gov"}
+		  end
+		end
+
+    class VSApiV2 < VSApi
+      def initialize(ticket_url, api_url, username, password)
+        super(ticket_url, api_url, username, password)
+      end
+
+      def get_valueset(oid, options = {}, &block)
+        version = options.fetch(:version, nil)
+        include_draft = options.fetch(:include_draft, false)
 				params = {id: oid, ticket: get_ticket}
 				params[:version] = version if version
 				params[:includeDraft] = 'yes' if include_draft
@@ -28,26 +66,14 @@ module HealthDataStandards
 				vs
 			end
 
-			def process_valuesets(oids, version=nil, &block)
+      def process_valuesets(oids, options = {}, &block)
+        version = options.fetch(:version, nil)
+        include_draft = options.fetch(:include_draft, false)
 				oids.each do |oid|
-		     		vs = get_valueset(oid,version)
-		     		yield oid,vs
+	        vs = get_valueset(oid, version: version, include_draft: include_draft)
+	    	  yield oid,vs
 				end
 			end
-
-			def proxy_ticket
-			 	@proxy_ticket ||= get_proxy_ticket
-			end
-
-			def get_proxy_ticket
-				# the content type is set and the body is a string becuase the NLM service does not support urlencoded content and
-				# throws an error on that contnet type
-				 RestClient.post ticket_url, {username: username, password: password}
-			end
-			
-			def get_ticket
-			  RestClient.post "#{ticket_url}/#{proxy_ticket}", {service: "http://umlsks.nlm.nih.gov"}
-		  end
-		end
+    end
   end
 end
