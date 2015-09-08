c2015c17398f665dda611ef3e3314e8cec26f0ce
David Czulada <dczulada@gmail.com>
2015-09-08 15:20:10 -0400

    Tests now use webmob removed hard coded url from NLM helper

--- lib/health-data-standards/util/nlm_helper.rb
+++ lib/health-data-standards/util/nlm_helper.rb
@@ -1,12 +1,11 @@
 require 'rest_client'
 require 'nokogiri'
-
 module HealthDataStandards
   module Util
     class NLMHelper
-      def self.validateNLMUser(proxy, nlmLicenseCode, nlmUser, nlmPassword)
+      def self.validateNLMUser(nlmUrl, proxy, nlmLicenseCode, nlmUser, nlmPassword)
         RestClient.proxy = proxy
-        nlmResult = RestClient.post 'https://uts-ws.nlm.nih.gov/restful/isValidUMLSUser', {user: nlmUser, password: nlmPassword, licenseCode: nlmLicenseCode}
+        nlmResult = RestClient.post nlmUrl, {user: nlmUser, password: nlmPassword, licenseCode: nlmLicenseCode}
         doc = Nokogiri::XML(nlmResult)
         return doc.search('Result').text == 'true'
       end
--- test/unit/util/nlm_helper_test.rb
+++ test/unit/util/nlm_helper_test.rb
@@ -1,11 +1,23 @@
 require 'test_helper'
+require 'webmock'
 
 class EntryTest < Minitest::Test
-  #Should fail
+  include WebMock::API
+
+  def initialize(name = nil)
+    $nlmUrl = "https://uts-ws.nlm.nih.gov/restful/isValidUMLSUser"
+    stub_request(:post,'https://uts-ws.nlm.nih.gov/restful/isValidUMLSUser').with(:body =>{"password"=>"mypassword", "user"=>"myusername", "licenseCode"=>"mylicense"}).to_return( :body=>"<?xml version='1.0' encoding='UTF-8'?><Result>true</Result>")
+    stub_request(:post,'https://uts-ws.nlm.nih.gov/restful/isValidUMLSUser').with(:body =>{"password"=>"notmypassword", "user"=>"myusername", "licenseCode"=>"mylicense"}).to_return( :body=>"<?xml version='1.0' encoding='UTF-8'?><Result>false</Result>")
+    super(name)
+  end
+  #Returns true when verification fails
   def test_validate_nlm_credentials
-  	WebMock.disable!
-    ts = HealthDataStandards::Util::NLMHelper.validateNLMUser('', 'wrong', 'username', 'password')
+    ts = HealthDataStandards::Util::NLMHelper.validateNLMUser($nlmUrl,'', 'mylicense', 'myusername', 'mypassword')
+    assert_equal true, ts
+  end
+  #Returns false when verification fails
+  def test_validate_nlm_credentials_fail
+    ts = HealthDataStandards::Util::NLMHelper.validateNLMUser($nlmUrl,'', 'mylicense', 'myusername', 'notmypassword')
     assert_equal false, ts
-    WebMock.enable!
   end
 end
