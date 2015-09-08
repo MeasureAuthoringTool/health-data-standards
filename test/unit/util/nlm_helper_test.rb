b5b192e0c56bc41cdbdad6926945b28d6fdb67be
David Czulada <dczulada@gmail.com>
2015-09-08 12:40:14 -0400

    disable webmock for test to allow for http connection

--- test/unit/util/nlm_helper_test.rb
+++ test/unit/util/nlm_helper_test.rb
@@ -3,7 +3,9 @@ require 'test_helper'
 class EntryTest < Minitest::Test
   #Should fail
   def test_validate_nlm_credentials
+  	WebMock.disable!
     ts = HealthDataStandards::Util::NLMHelper.validateNLMUser('', 'wrong', 'username', 'password')
     assert_equal false, ts
+    WebMock.enable!
   end
 end
