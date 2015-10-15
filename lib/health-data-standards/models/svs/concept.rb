e8f133f86f6460507d235f723ea1f91ba6a18af6
alexanderelliott121 <aelliott@mm191023-pc.mitre.org>
2015-10-15 08:55:36 -0400

    Update value sets to implement bonnie_version_hash and attendant functions. bonnie_version_hash replaces the user field for purposes of identification.

--- lib/health-data-standards/models/svs/concept.rb
+++ lib/health-data-standards/models/svs/concept.rb
@@ -12,6 +12,16 @@ module HealthDataStandards
       field :code_system, type: String
       scope :by_code_system_name,  ->(cs){where(code_system_name: cs)}
       scope :by_code_system,  ->(cs){where(code_system: cs)}
+      
+      def ==(comp)
+        if (self.code != comp.code)
+          return false
+        end
+        if (self.code_system_name != comp.code_system_name)
+          return false
+        end
+        return true
+      end
     end
   end
 end
