616a64e3f86cbb12684e097489dd58395d18d809
James Bradley <jhbradley@mitre.org>
2015-10-27 16:57:18 -0400

    Added Family History Support

--- lib/health-data-standards/models/family_history.rb
+++ lib/health-data-standards/models/family_history.rb
@@ -1,5 +1,6 @@
 class FamilyHistory < Entry
-
-  field :relationshipToPatient, as: :relationship_to_patient, type: String
+  field :relationshipToPatient, as: :relationship_to_patient, type: Hash
+  field :onsetAge, as: :onset_age, type: Hash
+  field :recordedDatetime, as: :recorded_datetime, type: Integer
 
 end
