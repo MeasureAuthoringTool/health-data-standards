f07558f18758df892a7e42eee550b1a0aeee6934
James Bradley <jhbradley@mitre.org>
2015-10-29 12:31:23 -0400

    Removing unused Family History Code

--- lib/health-data-standards/models/family_history.rb
+++ lib/health-data-standards/models/family_history.rb
@@ -1,6 +1,4 @@
 class FamilyHistory < Entry
   field :relationshipToPatient, as: :relationship_to_patient, type: Hash
   field :onsetAge, as: :onset_age, type: Hash
-  field :recordedDatetime, as: :recorded_datetime, type: Integer
-
 end
