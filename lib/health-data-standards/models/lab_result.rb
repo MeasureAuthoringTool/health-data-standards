e2b3618f748be770a78fe400a6357a1db56c0c1d
Pete K <krautscheid@mitre.org>
2015-10-30 17:10:34 -0400

    Updated tests to match new data criteria id structure, removed incorrect codes

--- lib/health-data-standards/models/lab_result.rb
+++ lib/health-data-standards/models/lab_result.rb
@@ -1,4 +1,5 @@
 class LabResult < Entry
+  field :referenceRange, as: :reference_range, type: String
   field :referenceRangeHigh, as: :reference_range_high, type: String
   field :referenceRangeLow, as: :reference_range_low, type: String
   field :interpretation, type: Hash  
