970fb8eaf8c6b8edf951528794f3938c876ccfc4
Chris Hossenlopp <hossenlopp@mitre.org>
2016-01-07 18:49:56 -0500

    Fixed include_draft option in VSApiV2. - Added a default profile parameter when using the include_draft option.   - The profile parameter is needed by VSAC V2 when using includeDraft. - Added an option to get_valuests so a profile can be explicity provided. - Fixed the class name of the VSApi tests to not conflict with another class of tests. - Added unit tests for the cases dealing with include_draft and profile.

--- test/unit/util/vs_api_test.rb
+++ test/unit/util/vs_api_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 require 'webmock'
-class EntryTest < Minitest::Test
+class VSApiTest < Minitest::Test
   include WebMock::API
 
   def initialize(name = nil)
@@ -54,6 +54,26 @@ class EntryTest < Minitest::Test
     stub_request(:get,'https://localhost/vsservice').with(:query =>{:id=>"oid", :ticket=>"ticket" ,:version => valueset_xml_version}).to_return(:body=>valueset_xml)
     api = HealthDataStandards::Util::VSApiV2.new("https://localhost/token", "https://localhost/vsservice", "myusername", "mypassword")
     api.get_valueset("oid", version: valueset_xml_version) do |oid,vs|
+      assert_equal "oid", oid
+      assert_equal valueset_xml, vs
+    end
+  end
+  
+  def test_api_v2_with_include_draft_default_profile
+    valueset_xml = %{<?xml version="1.0" encoding="UTF-8"?><RetrieveValueSetResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" cacheExpirationHint="2012-09-20T00:00:00-04:00" xmlns:nlm="urn:ihe:iti:svs:2008" xmlns="urn:ihe:iti:svs:2008"><ValueSet id="2.16.840.1.113883.11.20.9.23" version="Draft"></ValueSet></RetrieveValueSetResponse>}
+    stub_request(:get,'https://localhost/vsservice').with(:query =>{:id=>"oid", :ticket=>"ticket", :includeDraft=>"yes", :profile=>"Most Recent CS Versions"}).to_return(:body=>valueset_xml)
+    api = HealthDataStandards::Util::VSApiV2.new("https://localhost/token", "https://localhost/vsservice", "myusername", "mypassword")
+    api.get_valueset("oid", include_draft: true) do |oid,vs|
+      assert_equal "oid", oid
+      assert_equal valueset_xml, vs
+    end
+  end
+  
+  def test_api_v2_with_include_draft_specified_profile
+    valueset_xml = %{<?xml version="1.0" encoding="UTF-8"?><RetrieveValueSetResponse xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" cacheExpirationHint="2012-09-20T00:00:00-04:00" xmlns:nlm="urn:ihe:iti:svs:2008" xmlns="urn:ihe:iti:svs:2008"><ValueSet id="2.16.840.1.113883.11.20.9.23" version="Draft"></ValueSet></RetrieveValueSetResponse>}
+    stub_request(:get,'https://localhost/vsservice').with(:query =>{:id=>"oid", :ticket=>"ticket", :includeDraft=>"yes", :profile=>"Test Profile"}).to_return(:body=>valueset_xml)
+    api = HealthDataStandards::Util::VSApiV2.new("https://localhost/token", "https://localhost/vsservice", "myusername", "mypassword")
+    api.get_valueset("oid", include_draft: true, profile: "Test Profile") do |oid,vs|
       assert_equal "oid", oid
       assert_equal valueset_xml, vs
     end
