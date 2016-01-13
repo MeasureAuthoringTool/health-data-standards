5eca604305d6bbfe029521e1123f6e31b87da1de
Alexander Elliott <aelliott@mitre.org>
2016-01-13 11:38:21 -0500

    Add bonnie_version_hash check to before_save behavior.  Intent is to prevent duplicate bonnie_version_hash generation.

--- lib/health-data-standards/models/svs/value_set.rb
+++ lib/health-data-standards/models/svs/value_set.rb
@@ -8,12 +8,6 @@ module HealthDataStandards
       field :categories, type: Hash
       field :bonnie_version_hash, type: String #incoproates oid, version and concepts
 
-      def bonnie_version_hash
-        existing = super()
-        return existing if existing
-        self.bonnie_version_hash = HealthDataStandards::SVS::ValueSet.generate_bonnie_hash(self)
-      end
-
       belongs_to :bundle, class_name: "HealthDataStandards::CQM::Bundle", inverse_of: :value_sets
 
       index({oid: 1})
@@ -27,7 +21,9 @@ module HealthDataStandards
       scope :by_oid, ->(oid){where(:oid => oid)}
 
       before_save do |document|
-        document.bonnie_version_hash = HealthDataStandards::SVS::ValueSet.generate_bonnie_hash(document)
+        if (document.bonnie_version_hash.nil?)
+          document.bonnie_version_hash = HealthDataStandards::SVS::ValueSet.generate_bonnie_hash(document)
+        end
       end
 
       # Provides an Array of Hashes. Each code system gets its own Hash
