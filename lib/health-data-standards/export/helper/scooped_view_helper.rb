1e7003097aa6597de2e54a1857a10af17b3c2305
Czulada <dczulada@mm218917-pc.mitre.org>
2016-02-25 11:08:06 -0500

    strange character issues

--- lib/health-data-standards/export/helper/scooped_view_helper.rb
+++ lib/health-data-standards/export/helper/scooped_view_helper.rb
@@ -147,13 +147,13 @@ module HealthDataStandards
                 if (entry.transferFrom)
                   entry.transferFrom.codes[entry.transferFrom.code_system] = [entry.transferFrom.code]
                   tfc = entry.transferFrom.codes_in_code_set(codes).values.first
-                  tfc && !tfc.empty?
+                  tfc && !tfc.empty?
                 end
               elsif data_criteria_oid == '2.16.840.1.113883.3.560.1.72'
                 if (entry.transferTo)
                   entry.transferTo.codes[entry.transferTo.code_system] = [entry.transferTo.code]
                   ttc = entry.transferTo.codes_in_code_set(codes).values.first
-                  ttc && !ttc.empty?
+                  ttc && !ttc.empty?
                 end
               else
                 # The !! hack makes sure that negation_ind is a boolean
