915f50eb9852ff7dfa91d6d2fe498250d0090b4e
Lizzie DeYoung <edeyoung@mitre.org>
2016-07-27 08:40:48 -0400

    added 'qdm_status' to lab results and procedures. Note: 'qdm_status' is used here to differentiate from the 'status' attribute on an entry, which has a different meaning than this status (e.g. performed, completed, etc.)

--- lib/health-data-standards/models/procedure.rb
+++ lib/health-data-standards/models/procedure.rb
@@ -7,6 +7,10 @@ class Procedure < Entry
   field :anatomical_target,   type: Hash
   field :method,              type: Hash
   field :reaction,            type: Hash 
+  # The resulting status of a procedure as defined in the QDM documentation. This is different
+  # than the status associated with the `Entry` object, which relates to the data criteria
+  # status as defined in health-data-standards/lib/hqmf-model/data_criteria.json.
+  field :qdm_status,          type: Hash
 
   field :radiation_dose,      type: Hash 
   field :radiation_duration,  type: Hash 
