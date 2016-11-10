96c912f8af38e230704c18a6e1e97a9ff6a11664
Czulada, David S <dczulada@mitre.org>
2016-11-10 15:09:14 -0500

    added act wrapper templates to encounter, transfers and medication dispense
    extract negations and remove duplicative global variables in importers
    export negations in act templates

---
+++ lib/health-data-standards/import/cat1/transfer_from_act_importer.rb
@@ -0,0 +1,18 @@
+module HealthDataStandards
+  module Import
+    module Cat1
+      class TransferFromActImporter < TransferFromImporter
+
+        def initialize
+          super(CDA::EntryFinder.new("./cda:entry/cda:act[./cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.141']/cda:entryRelationship/cda:encounter[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.81']"))
+        end
+
+        def create_entry(entry_element, nrh = CDA::NarrativeReferenceHandler.new)
+          encounter = super
+          extract_reason_or_negation(entry_element.parent.parent, encounter)
+          encounter
+        end
+      end
+    end
+  end
+end
