d4df2316ef921f6264cc32bd49bc221358b2b8e9
Czulada, David S <dczulada@mitre.org>
2016-11-15 15:54:48 -0500

    extract negations and remove duplicative global variables in importers export negations in act templates

--- lib/health-data-standards/import/cat1/encounter_performed_act_importer.rb
+++ lib/health-data-standards/import/cat1/encounter_performed_act_importer.rb
@@ -5,10 +5,14 @@ module HealthDataStandards
 
         def initialize
           super(CDA::EntryFinder.new("./cda:entry/cda:act[./cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.133']/cda:entryRelationship/cda:encounter[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.23']"))
-          @reason_xpath = "./cda:entryRelationship[@typeCode='RSON']/cda:observation"
-          @principal_diagnosis_xpath = "./cda:entryRelationship/cda:observation[cda:code/@code='8319008']"
-          @diagnosis_xpath = "./cda:entryRelationship/cda:act/cda:entryRelationship/cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.4']"
         end
+
+        def create_entry(entry_element, nrh = CDA::NarrativeReferenceHandler.new)
+          encounter = super
+          extract_reason_or_negation(entry_element.parent.parent, encounter)
+          encounter
+        end
+
       end
     end
   end
--- lib/health-data-standards/import/cat1/medication_dispensed_act_importer.rb
+++ lib/health-data-standards/import/cat1/medication_dispensed_act_importer.rb
@@ -6,6 +6,12 @@ module HealthDataStandards
         def initialize
           super(CDA::EntryFinder.new("./cda:entry/cda:act[./cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.139']/cda:entryRelationship/cda:supply[cda:templateId/@root='2.16.840.1.113883.10.20.24.3.45']"))
         end
+
+        def create_entry(entry_element, nrh = CDA::NarrativeReferenceHandler.new)
+          medication = super
+          extract_reason_or_negation(entry_element.parent.parent, medication)
+          medication
+        end
       end
     end
   end
