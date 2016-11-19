1117143edc7b38f655352e734fa43acd10884fff
Dillon Welch <daw0328@gmail.com>
2016-11-18 17:48:40 -0800

    Fix formatting on module

--- lib/health-data-standards/models/facility.rb
+++ lib/health-data-standards/models/facility.rb
@@ -1,19 +1,21 @@
-class HealthDataStandards::Facility
-  include Mongoid::Document
-  include Mongoid::Attributes::Dynamic
+module HealthDataStandards
+  class Facility
+    include Mongoid::Document
+    include Mongoid::Attributes::Dynamic
 
-  field :name, type: String
-  field :code, type: Hash
+    field :name, type: String
+    field :code, type: Hash
 
-  field :start_time, type: Integer
-  field :end_time, type: Integer
+    field :start_time, type: Integer
+    field :end_time, type: Integer
 
-  embeds_many :addresses, as: :locatable
-  embeds_many :telecoms, as: :contactable
+    embeds_many :addresses, as: :locatable
+    embeds_many :telecoms, as: :contactable
 
-   def shift_dates(date_diff)
-    self.start_time = (self.start_time.nil?) ? nil : self.start_time + date_diff
-    self.end_time = (self.end_time.nil?) ? nil : self.end_time + date_diff
+     def shift_dates(date_diff)
+      self.start_time = (self.start_time.nil?) ? nil : self.start_time + date_diff
+      self.end_time = (self.end_time.nil?) ? nil : self.end_time + date_diff
+    end
   end
 end
 
