ce9b908c5c894eab666627e5490382af3577e813
Czulada, David S <dczulada@mitre.org>
2016-11-23 11:44:08 -0500

    catch for unsupported qrda versions

--- test/unit/validate/qrda_qdm_template_validator_test.rb
+++ test/unit/validate/qrda_qdm_template_validator_test.rb
@@ -5,6 +5,13 @@ class QrdaQdmTemplateValidatorTest < ActiveSupport::TestCase
   setup do
     @validator_r3 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r3')
     @validator_r3_1 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r3_1')
+    @validator_r4 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r4')
+  end
+
+  test "Should not produce errors if validator does not support version specified" do
+    xml = File.open("./test/fixtures/qrda/cat1_r3_good.xml","r") do |f| f.read() end
+    errors = @validator_r4.validate(xml)
+    assert_equal  0,  errors.length, "File should not contain any errors"
   end
 
   test "Should not produce errors for good file" do
