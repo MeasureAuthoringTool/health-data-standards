f53d77f7e484cc4dd1df3225f064d7c70ed7cf23
Michael O'Keefe <mokeefe@mitre.org>
2016-12-16 08:19:43 -0500

    Added tests for encounter, order; encounter, performed; and medication, dispensed act template importers

--- test/unit/import/cat1/encounter_transfer_importer_test.rb
+++ test/unit/import/cat1/encounter_transfer_importer_test.rb
@@ -6,7 +6,23 @@ class EncounterTransferImporterTest < Minitest::Test
     doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
     nrh = HealthDataStandards::Import::CDA::NarrativeReferenceHandler.new
     nrh.build_id_map(doc)
-    enc = HealthDataStandards::Import::Cat1::EntryPackage.new(HealthDataStandards::Import::CDA::EncounterImporter.new(HealthDataStandards::Import::CDA::EntryFinder.new("//cda:encounter[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.23']")), '2.16.840.1.113883.3.560.1.79', 'performed')
+    enc = HealthDataStandards::Import::Cat1::EntryPackage.new(HealthDataStandards::Import::Cat1::EncounterPerformedImporter.new(HealthDataStandards::Import::CDA::EntryFinder.new("//cda:encounter[cda:templateId/@root = '2.16.840.1.113883.10.20.24.3.23']")), '2.16.840.1.113883.3.560.1.79', 'performed')
+    encounters = enc.package_entries(cat1_patient_data_section(doc), nrh)
+    encounter = encounters[0]
+    from_time = HealthDataStandards::Util::HL7Helper.timestamp_to_integer('20120214')
+    to_time = HealthDataStandards::Util::HL7Helper.timestamp_to_integer('20120214')
+    assert encounter.transferFrom['code'].include?("1024-9")
+    assert encounter.transferTo['code'].include?("1194-0")
+    assert_equal encounter.transferFrom.time, from_time
+    assert_equal encounter.transferTo.time, to_time
+  end
+
+  def test_encounter_transfer_act_importing
+    doc = Nokogiri::XML(File.new('test/fixtures/cat1_fragments/encounter_performed_act_fragment.xml'))
+    doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+    nrh = HealthDataStandards::Import::CDA::NarrativeReferenceHandler.new
+    nrh.build_id_map(doc)
+    enc = HealthDataStandards::Import::Cat1::EntryPackage.new(HealthDataStandards::Import::Cat1::EncounterPerformedActImporter.new, '2.16.840.1.113883.3.560.1.79', 'performed')
     encounters = enc.package_entries(cat1_patient_data_section(doc), nrh)
     encounter = encounters[0]
     from_time = HealthDataStandards::Util::HL7Helper.timestamp_to_integer('20120214')
