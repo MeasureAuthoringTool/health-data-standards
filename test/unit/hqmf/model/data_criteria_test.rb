7c53194a5cb0eda07626f6b41521adfdf5c522f4
Lizzie DeYoung <edeyoung@mitre.org>
2017-02-14 16:03:49 -0500

    changed test to not only ignore mismatches but to expect particular mismatches.

--- test/unit/hqmf/model/data_criteria_test.rb
+++ test/unit/hqmf/model/data_criteria_test.rb
@@ -16,11 +16,12 @@ module HQMFModel
         mismatches << field_title unless field == title
       end
 
-      # ignore custom fulfills and previous ordinal field values
-      mismatches -= ["FLFS - FULFILLS", "ORDINAL - ORDINALITY"]
-
-      # ignore changes needed when converting to qdm 5.0
-      mismatches -= [
+      
+      expected_mismatches = [
+        # ignore custom fulfills and previous ordinal field values
+        "FLFS - FULFILLS",
+        "ORDINAL - ORDINALITY",
+        # the following represent display name changes that were necessary for the QDM 5.0 update
         "DISCHARGE_STATUS - DISCHARGE_DISPOSITION",
         "DOSE - DOSAGE",
         "FACILITY_LOCATION_ARRIVAL_DATETIME - LOCATION_PERIOD_START_DATETIME",
@@ -28,9 +29,7 @@ module HQMFModel
         "REFERENCE_RANGE_HIGH - REFERENCE_RANGE_-_HIGH",
         "REFERENCE_RANGE_LOW - REFERENCE_RANGE_-_LOW"
       ]
-
-      assert mismatches.empty?, "Mismatches found: #{mismatches}."
-
+      assert_equal mismatches.sort, expected_mismatches.sort
     end
 
     def test_value_fields
