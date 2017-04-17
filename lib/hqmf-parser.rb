624af1adfba75e21459c884b035cc20390c9ee35
Matt Mayer <mmayer@mitre.org>
2017-04-17 16:56:47 -0400

    Remove require statement of removed file

--- lib/hqmf-parser.rb
+++ lib/hqmf-parser.rb
@@ -36,7 +36,6 @@ require_relative 'hqmf-parser/2.0/data_criteria'
 require_relative 'hqmf-parser/2.0/population_criteria'
 require_relative 'hqmf-parser/2.0/precondition'
 
-require_relative 'hqmf-parser/cql/population_helper'
 require_relative 'hqmf-parser/cql/document'
 require_relative 'hqmf-parser/cql/document_helpers/doc_population_helper'
 
