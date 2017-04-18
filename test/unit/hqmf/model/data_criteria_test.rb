74eeb3c75072c445c2083fd80cfd7f7b12f33092
edeyoung <edeyoung@mitre.org>
2017-04-18 09:04:01 -0400

    several displayed descriptions for data criteria needed to change to … (#475)
    * several displayed descriptions for data criteria needed to change to conform with QDM 5.0 changes. However, the codes themselves seemed like they should stay the same for backwards compatibility. this led to discrepencies in the testing. these are addressed in this pull request.
    
    * changed test to not only ignore mismatches but to expect particular mismatches.

--- test/unit/hqmf/model/data_criteria_test.rb
+++ test/unit/hqmf/model/data_criteria_test.rb
@@ -16,11 +16,20 @@ module HQMFModel
         mismatches << field_title unless field == title
       end
 
-      # ignore custom fulfills and previous ordinal field values
-      mismatches -= ["FLFS - FULFILLS", "ORDINAL - ORDINALITY"]
-
-      assert mismatches.empty?, "Mismatches found: #{mismatches}."
-
+      
+      expected_mismatches = [
+        # ignore custom fulfills and previous ordinal field values
+        "FLFS - FULFILLS",
+        "ORDINAL - ORDINALITY",
+        # the following represent display name changes that were necessary for the QDM 5.0 update
+        "DISCHARGE_STATUS - DISCHARGE_DISPOSITION",
+        "DOSE - DOSAGE",
+        "FACILITY_LOCATION_ARRIVAL_DATETIME - LOCATION_PERIOD_START_DATETIME",
+        "FACILITY_LOCATION_DEPARTURE_DATETIME - LOCATION_PERIOD_END_DATETIME",
+        "REFERENCE_RANGE_HIGH - REFERENCE_RANGE_-_HIGH",
+        "REFERENCE_RANGE_LOW - REFERENCE_RANGE_-_LOW"
+      ]
+      assert_equal mismatches.sort, expected_mismatches.sort
     end
 
     def test_value_fields
