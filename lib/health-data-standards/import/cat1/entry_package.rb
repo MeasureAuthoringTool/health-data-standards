fe026a3d7854aa815371ab372b86ccc5c5efbf49
Saravana kumar J <saravana.k@unislink.com>
2017-05-02 15:56:49 +0530

    entrypackage - negationInd changes;

--- lib/health-data-standards/import/cat1/entry_package.rb
+++ lib/health-data-standards/import/cat1/entry_package.rb
@@ -18,11 +18,8 @@ module HealthDataStandards
             entry.oid = self.hqmf_oid
             entry.status = self.status if self.status
             if entry.negationInd
-                oid_status = (entry.status || "")
-                oid_definition = entry._type.to_s.downcase 
-                oid_definition = entry.direction if (entry._type.to_s.downcase == "communication" && entry.direction)
-                oid_definition = "diagnosis" if entry._type.to_s.downcase == "condition" 
-                entry.oid = (HQMFTemplateHelper.template_id_by_definition_and_status(oid_definition, oid_status, entry.negationInd) || self.hqmf_oid)
+                definition = HQMFTemplateHelper.definition_for_template_id(self.hqmf_oid)
+                entry.oid = HQMFTemplateHelper.template_id_by_definition_and_status(definition["definition"], definition["status"], true) if definition
             end
           end          
           entries
