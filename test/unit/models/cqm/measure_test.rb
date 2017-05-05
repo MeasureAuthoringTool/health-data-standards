6526b78b0552c6b7cd787302001b53f3fc26bfba
Czulada, David S <dczulada@mitre.org>
2017-05-05 13:57:18 -0400

    Add QRDA Cat I R4 schematron validation to cat_1_test

--- test/unit/models/cqm/measure_test.rb
+++ test/unit/models/cqm/measure_test.rb
@@ -11,7 +11,7 @@ class MeasureTest < ActiveSupport::TestCase
 
   def test_categories
     categories = HealthDataStandards::CQM::Measure.categories
-    assert categories.any? {|c| c['category'] == 'Core' && c['measures'].size == 1}
+    assert categories.any? {|c| c['category'] == 'Core' && c['measures'].size == 2}
     assert categories.any? {|c| c['category'] == 'General Practice Adult' && c['measures'].size == 1}
     assert categories.any? do |c|
       c['category'] == 'General Practice Adult' &&
@@ -31,7 +31,7 @@ class MeasureTest < ActiveSupport::TestCase
   test "Should list installed measures" do
     measures = HealthDataStandards::CQM::Measure.installed
 
-    assert_equal 23, measures.count, "there should be 23 measures"
+    assert_equal 24, measures.count, "there should be 24 measures"
     assert measures.index{|m| m.measure_id=="0001"} != nil
     assert measures.index{|m| m.measure_id=="0002"} != nil
     assert measures.index{|m| m.measure_id=="0348"} != nil
@@ -40,7 +40,7 @@ class MeasureTest < ActiveSupport::TestCase
   test "Should list top levels" do
     measures = HealthDataStandards::CQM::Measure.top_level
 
-    assert_equal 12, measures.count, "should count 12 top level measures "
+    assert_equal 13, measures.count, "should count 13 top level measures "
     assert measures.where(:hqmf_id=>"0001").count() == 1, "Top level measure 0001 Not Found"
     assert measures.where(:hqmf_id=>"0002").count() == 1, "Top level measure 0002 Not Found"
     assert measures.where(:hqmf_id=>"0348").count() == 1, "Top level measure 0348 Not Found"
