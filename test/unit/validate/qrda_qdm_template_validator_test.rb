7507be9648641cd0278d8b5dd3a4b7233db6155d
Czulada, David S <dczulada@mitre.org>
2017-06-27 12:09:19 -0400

    Add support for CAT I R4 in QDM Template Validator
    r4 is no longer an unsupported version.  change to 1 so this doesn't happen when new versions are added

--- test/unit/validate/qrda_qdm_template_validator_test.rb
+++ test/unit/validate/qrda_qdm_template_validator_test.rb
@@ -5,12 +5,12 @@ class QrdaQdmTemplateValidatorTest < ActiveSupport::TestCase
   setup do
     @validator_r3 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r3')
     @validator_r3_1 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r3_1')
-    @validator_r4 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r4')
+    @validator_r1 = HealthDataStandards::Validate::QrdaQdmTemplateValidator.new('r1')
   end
 
   test "Should not produce errors if validator does not support version specified" do
     xml = File.open("./test/fixtures/qrda/cat1_r3_good.xml","r") do |f| f.read() end
-    errors = @validator_r4.validate(xml)
+    errors = @validator_r1.validate(xml)
     assert_equal  0,  errors.length, "File should not contain any errors"
   end
 
