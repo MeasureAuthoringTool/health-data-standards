bb58a1667efffe14b49a32d3006b1784d75a8e58
Luke Osborne <luke.w.osborne@gmail.com>
2017-07-15 19:19:58 -0400

    Add code_system, title fields to AdmissionSource

--- lib/health-data-standards/models/admission_source.rb
+++ lib/health-data-standards/models/admission_source.rb
@@ -5,5 +5,7 @@ class AdmissionSource
   embedded_in :encounter, class_name: "Encounter", inverse_of: nil
   
   field :code, type: String
-
+  field :code_system, type: String
+  field :title, type: String
+  
 end
