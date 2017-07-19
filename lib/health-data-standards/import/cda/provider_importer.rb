7c7261e22c34808ab6af85e0ca462b9b8e7e2d1c
Saravana kumar J <saravana.k@unislink.com>
2017-07-19 10:53:57 +0530

    attributed provider from informat;

--- lib/health-data-standards/import/cda/provider_importer.rb
+++ lib/health-data-standards/import/cda/provider_importer.rb
@@ -26,7 +26,58 @@ module HealthDataStandards
           end
         end
 
+        def extract_informant_provider(doc)
+          informant = doc.xpath("//cda:informant[position()=1]")
+          informant_prov = extract_informant_provider_data(informant)
+          provider = find_or_create_provider(informant_prov)
+          provider.parent = practice_provider if provider
+          provider
+        end
+
         private
+
+        def extract_informant_provider_data(informant, use_dates=true, entity_path="./cda:assignedEntity")
+          provider = {}
+          entity = informant.xpath(entity_path)
+
+          cda_idents = []
+          entity.xpath("./cda:id").each do |cda_ident|
+            ident_root = cda_ident['root']
+            ident_extension = cda_ident['extension']
+            cda_idents.push(CDAIdentifier.new(root: ident_root, extension: ident_extension)) if ident_root.present?
+          end
+
+          #6706 - Added below section to import TIN - "2.16.840.1.113883.4.2"
+          entity.xpath("./cda:representedOrganization/cda:id").each do |cda_ident|
+	          ident_root = cda_ident['root']
+	          ident_extension = cda_ident['extension']
+	          cda_idents.push(CDAIdentifier.new(root: ident_root, extension: ident_extension)) if ident_root.present?
+	        end
+
+          name = entity.at_xpath("./cda:assignedPerson/cda:name")
+          provider[:title]        = extract_data(name, "./cda:prefix")
+          provider[:given_name]   = extract_data(name, "./cda:given[1]")
+          provider[:family_name]  = extract_data(name, "./cda:family[1]")
+          provider[:organization] = OrganizationImporter.instance.extract_organization(entity.at_xpath("./cda:representedOrganization"))
+          provider[:specialty]    = extract_data(entity, "./cda:code/@code")
+          time                    = informant.xpath(informant, "./cda:time")
+          time                    = informant.xpath(informant, "../cda:effectiveTime") if time.nil? || time.empty?
+          
+          if use_dates
+            provider[:start]        = extract_date(time, "./cda:low/@value")
+            provider[:end]          = extract_date(time, "./cda:high/@value")
+          end
+          
+          # NIST sample C32s use different OID for NPI vs C83, support both
+          npi                     = extract_data(entity, "./cda:id[@root='2.16.840.1.113883.4.6' or @root='2.16.840.1.113883.3.72.5.2']/@extension")
+          provider[:addresses] = performer.xpath("./cda:assignedEntity/cda:addr").try(:map) {|ae| import_address(ae)}
+          provider[:telecoms] = performer.xpath("./cda:assignedEntity/cda:telecom").try(:map) {|te| import_telecom(te)}
+          
+          provider[:npi] = npi if Provider.valid_npi?(npi)
+          provider[:cda_identifiers] = cda_idents
+
+          provider
+        end
       
         def extract_provider_data(performer, use_dates=true, entity_path="./cda:assignedEntity")
           provider = {}
