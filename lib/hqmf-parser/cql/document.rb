9a015e4860fb7a0b35ca03694ca287b4eb574d2d
mulcahyk <kmulcahy@mitre.org>
2017-08-07 17:45:57 -0400

    Tighten code based on feedback.

--- lib/hqmf-parser/cql/document.rb
+++ lib/hqmf-parser/cql/document.rb
@@ -52,29 +52,26 @@ module HQMF2CQL
     # is only the negative one in the HQMF.
     def make_positive_entry
       negated_criteria = []
-      description_hash = {}
-      data_criteria_index_lookup = {}
+      data_criteria_index_lookup = []
       # Find the criteria that are negated
       # At the same time build a hash of all criteria and their code_list_id, definition, status, and negation status
       @data_criteria.each_with_index do |criterion, source_index|
         negated_criteria << criterion if criterion.negation
-        data_criteria_index_lookup[source_index] = [criterion.code_list_id, criterion.definition, criterion.status, criterion.negation]
+        data_criteria_index_lookup << [criterion.code_list_id, criterion.definition, criterion.status, criterion.negation]
       end
 
-      unless negated_criteria.count == 0
-        negated_criteria.each do |criterion|
-          # Check if there is a criterion with the same OID, definition and status BUT that isn't negated
-          unless data_criteria_index_lookup.value?([criterion.code_list_id, criterion.definition, criterion.status, false])
-            spoofed_positive_instance = criterion.clone
-            spoofed_positive_instance.make_criterion_positive
-            @data_criteria << spoofed_positive_instance
-            sdc = spoofed_positive_instance.clone
-            sdc.id += '_source'
-            @source_data_criteria << sdc
-          end
+      negated_criteria.each do |criterion|
+        # Check if there is a criterion with the same OID, definition and status BUT that isn't negated
+        unless data_criteria_index_lookup.include?([criterion.code_list_id, criterion.definition, criterion.status, false])
+          spoofed_positive_instance = criterion.clone
+          spoofed_positive_instance.make_criterion_positive
+          @data_criteria << spoofed_positive_instance
+          sdc = spoofed_positive_instance.clone
+          sdc.id += '_source'
+          @source_data_criteria << sdc
         end
-
       end
+
     end
 
   end
