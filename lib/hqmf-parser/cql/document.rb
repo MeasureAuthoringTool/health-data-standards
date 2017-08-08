2c2aea97734232546cb910c59d6f679a445163ee
mulcahyk <kmulcahy@mitre.org>
2017-08-08 09:38:44 -0400

    Add logic that will find where there is only a negated data criterion… (#525)
    * Add logic that will find where there is only a negated data criterion and spoof a positive one from the negated one.

--- lib/hqmf-parser/cql/document.rb
+++ lib/hqmf-parser/cql/document.rb
@@ -42,6 +42,36 @@ module HQMF2CQL
         @data_criteria << dc
         @source_data_criteria << sdc
       end
+      make_positive_entry
+    end
+
+    # This method is needed for situations when there is a only
+    # a negated version of a data criteria.  Bonnie will only
+    # show the affirmative version of data criteria.  This method
+    # will create an affirmative version of a data criteria when there
+    # is only the negative one in the HQMF.
+    def make_positive_entry
+      negated_criteria = []
+      data_criteria_index_lookup = []
+      # Find the criteria that are negated
+      # At the same time build a hash of all criteria and their code_list_id, definition, status, and negation status
+      @data_criteria.each_with_index do |criterion, source_index|
+        negated_criteria << criterion if criterion.negation
+        data_criteria_index_lookup << [criterion.code_list_id, criterion.definition, criterion.status, criterion.negation]
+      end
+
+      negated_criteria.each do |criterion|
+        # Check if there is a criterion with the same OID, definition and status BUT that isn't negated
+        unless data_criteria_index_lookup.include?([criterion.code_list_id, criterion.definition, criterion.status, false])
+          spoofed_positive_instance = criterion.clone
+          spoofed_positive_instance.make_criterion_positive
+          @data_criteria << spoofed_positive_instance
+          sdc = spoofed_positive_instance.clone
+          sdc.id += '_source'
+          @source_data_criteria << sdc
+        end
+      end
+
     end
 
   end
