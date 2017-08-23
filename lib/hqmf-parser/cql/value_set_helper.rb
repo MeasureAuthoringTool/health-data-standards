7ed5291894f94f93199655059afad9945cb83e5d
Lizzie DeYoung <edeyoung@mitre.org>
2017-08-23 17:29:14 -0400

    added missing templates that are present in the 2.0/value_set_helper.rb. This enables the value sets to be found.

--- lib/hqmf-parser/cql/value_set_helper.rb
+++ lib/hqmf-parser/cql/value_set_helper.rb
@@ -90,7 +90,9 @@ module HQMF2CQL
       '2.16.840.1.113883.10.20.28.4.115' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role/cda:playingMaterial[@classCode='MMAT']/cda:code", result_path: nil },
       '2.16.840.1.113883.10.20.28.4.116' => { valueset_path: './*/cda:value', result_path: nil },
       '2.16.840.1.113883.10.20.28.4.117' => { valueset_path: './*/cda:code', result_path: './*/cda:value' },
-      '2.16.840.1.113883.10.20.28.4.118' => { valueset_path: './*/cda:code', result_path: nil }
+      '2.16.840.1.113883.10.20.28.4.118' => { valueset_path: './*/cda:code', result_path: nil },
+      '2.16.840.1.113883.10.20.28.4.119' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role[@classCode='MANU']/cda:playingEntity[@classCode='MMAT']/cda:code", result_path: nil },
+      '2.16.840.1.113883.10.20.28.4.120' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role[@classCode='MANU']/cda:playingEntity[@classCode='MMAT']/cda:code", result_path: nil }
     }
     # rubocop:enable Metrics/LineLength
 
