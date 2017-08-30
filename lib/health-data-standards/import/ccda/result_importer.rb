15d553a1ebf2e8354c93bc7b6ccc99cc2d7f2576
Saravana kumar J <saravana.k@unislink.com>
2017-08-30 12:12:46 +0530

    CDA: result importer for extract codes

--- lib/health-data-standards/import/ccda/result_importer.rb
+++ lib/health-data-standards/import/ccda/result_importer.rb
@@ -5,6 +5,23 @@ module HealthDataStandards
         def initialize
           super(CDA::EntryFinder.new("//cda:observation[cda:templateId/@root='2.16.840.1.113883.10.20.22.4.2']"))
         end
+
+        def extract_codes(parent_element, entry)
+          super
+          
+          if !entry.codes.present?
+            code_elements = parent_element.xpath("../../cda:code")
+            code_elements.each do |code_element|
+              add_code_if_present(code_element, entry)
+              translations = code_element.xpath('cda:translation')
+              translations.each do |translation|
+                add_code_if_present(translation, entry)
+              end
+            end
+          end
+
+        end
+
       end
     end
   end
