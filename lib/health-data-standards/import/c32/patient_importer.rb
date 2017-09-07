6bcd230951db99688a1c3c613b7394d9582e8680
Saravana kumar J <saravana.k@unislink.com>
2017-09-07 18:00:36 +0530

    added log - patient importer;

--- lib/health-data-standards/import/c32/patient_importer.rb
+++ lib/health-data-standards/import/c32/patient_importer.rb
@@ -115,6 +115,7 @@ module HealthDataStandards
           nrh = CDA::NarrativeReferenceHandler.new
           nrh.build_id_map(doc)
           @section_importers.each_pair do |section, importers|
+            Rails.logger.info "#{@patient_identifier} #{section} starts #{Time.now.utc}"
             importers.each do |importer|
               entries = importer.package_entries(doc, nrh) if defined? importer.package_entries
               entries = importer.create_entries(doc, nrh)  if defined? importer.create_entries
@@ -130,7 +131,8 @@ module HealthDataStandards
         # @param [Nokogiri::XML::Node] doc The C32 document parsed by Nokogiri
         def get_demographics(patient, doc)
           entity_node = doc.at_xpath('/cda:ClinicalDocument/cda:id')
-          patient[:entity_id] = entity_node['extension'] if entity_node        
+          patient[:entity_id] = entity_node['extension'] if entity_node
+          @patient_identifier = patient[:entity_id] || "ENTITY_ID_UNK"        
           effective_date = doc.at_xpath('/cda:ClinicalDocument/cda:effectiveTime')['value']
           patient.effective_time = HL7Helper.timestamp_to_integer(effective_date)
           patient_role_element = doc.at_xpath('/cda:ClinicalDocument/cda:recordTarget/cda:patientRole')
