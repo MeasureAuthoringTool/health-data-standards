1a7588e2cbe7991519b61459da35c624c707c813
Saravana Kumar J <saravana.k@unislink.com>
2018-02-06 13:35:58 +0530

    import - payer type;

--- lib/health-data-standards/import/ccda/insurance_provider_importer.rb
+++ lib/health-data-standards/import/ccda/insurance_provider_importer.rb
@@ -15,7 +15,15 @@ module HealthDataStandards
           value_element = payer_element.at_xpath('cda:value')
           ip.codes = { 'SOP' => [value_element['code']] } if value_element
           extract_dates(payer_element, ip)
+          extract_payer_type(payer_element, ip)
           ip
+        end
+
+        def extract_payer_type(payer_element, ip)
+          ins_type_element = payer_element.at_xpath("./cda:id[@root='InsuranceType']")
+          if ip.payer && ins_type_element && ins_type_element['extension']
+            ip.payer['type'] = ins_type_element['extension']
+          end          
         end
 
       end
