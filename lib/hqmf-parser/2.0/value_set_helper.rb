8afa4ba276a47f08ad46a941c167a5115b1627f1
hossenlopp <hossenlopp@mitre.org>
2018-04-26 10:14:50 -0400

    adding participation data type (#567) (#571)
    * adding participation data type
    
    * added 's' to plural of participation

--- lib/hqmf-parser/2.0/value_set_helper.rb
+++ lib/hqmf-parser/2.0/value_set_helper.rb
@@ -92,8 +92,8 @@ module HQMF2
       '2.16.840.1.113883.10.20.28.3.117' => { valueset_path: './*/cda:code', result_path: './*/cda:value' },
       '2.16.840.1.113883.10.20.28.3.118' => { valueset_path: './*/cda:code', result_path: nil },
       '2.16.840.1.113883.10.20.28.3.119' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role[@classCode='MANU']/cda:playingEntity[@classCode='MMAT']/cda:code", result_path: nil },
-      '2.16.840.1.113883.10.20.28.3.120' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role[@classCode='MANU']/cda:playingEntity[@classCode='MMAT']/cda:code", result_path: nil }
-
+      '2.16.840.1.113883.10.20.28.3.120' => { valueset_path: "./*/cda:participation[@typeCode='CSM']/cda:role[@classCode='MANU']/cda:playingEntity[@classCode='MMAT']/cda:code", result_path: nil },
+      '2.16.840.1.113883.10.20.28.3.130' => { valueset_path: './*/cda:value', result_path: nil }
     }
     # rubocop:enable Metrics/LineLength
 
