1c91dfd2875b3c8d41f2c134fe00a818bc3706e3
Matt Mayer <mmayer@mitre.org>
2018-06-20 12:55:23 -0400

    try reverting import files

--- lib/health-data-standards/import/cat1/entry_package.rb
+++ lib/health-data-standards/import/cat1/entry_package.rb
@@ -13,24 +13,11 @@ module HealthDataStandards
 
         def package_entries (doc, nrh)
           entries = self.importer_type.create_entries(doc, nrh)
-          entries_to_add = []
-          entries_to_delete = []
-          entries.each_with_index do |entry, index|
-            if self.hqmf_oid.kind_of?(Array)
-              self.hqmf_oid.each do |hqmf_oid|
-                entry_copy = entry.clone
-                entry_copy.oid = hqmf_oid
-                entry_copy.status = self.status
-                entry_copy.cda_identifier['extension'] = entry_copy.id.to_s
-                entries_to_add << entry_copy
-              end
-              entries_to_delete << entry
-            else
-              entry.oid = self.hqmf_oid
-              entry.status = self.status
-            end
-          end
-          entries.concat(entries_to_add) - entries_to_delete
+          entries.each do |entry| 
+            entry.oid = self.hqmf_oid
+            entry.status = self.status
+          end          
+          entries
         end
       end
     end
