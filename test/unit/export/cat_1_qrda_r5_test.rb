2d2c0230ac7f16dde1807e229481bfb20f8fcf93
Lizzie DeYoung <edeyoung@mitre.org>
2018-07-27 14:31:34 -0400

    added functionality to show multiple facilities. made facility start/end look at the actual facility start/end. added additional tests.

--- test/unit/export/cat_1_qrda_r5_test.rb
+++ test/unit/export/cat_1_qrda_r5_test.rb
@@ -37,6 +37,8 @@ class Cat1TestQRDAR5 < Minitest::Test
     encounter_performed_xpath = get_entry_xpath("2.16.840.1.113883.10.20.24.3.23")
     encounter_performed_node = @doc.xpath(encounter_performed_xpath)
 
+    assert_equal 1, encounter_performed_node.count
+
     # relevant period
     relevant_period_node = encounter_performed_node.xpath("./xmlns:act/xmlns:entryRelationship/xmlns:encounter/xmlns:effectiveTime")
     start = relevant_period_node.xpath("./xmlns:low/@value")
@@ -55,25 +57,41 @@ class Cat1TestQRDAR5 < Minitest::Test
 
     kidney_failure = diagnoses_nodes.xpath("./xmlns:entryRelationship/xmlns:observation/xmlns:value[@code=\"129151000119102\"]")
     proteinuria = diagnoses_nodes.xpath("./xmlns:entryRelationship/xmlns:observation/xmlns:value[@code=\"12178007\"]")
-
     assert kidney_failure != nil
     assert proteinuria != nil
 
     # discharge disposition
-
-    # length of stay
+    discharge_disposition_node = encounter_performed_node.xpath("./xmlns:act/xmlns:entryRelationship/xmlns:encounter/sdtc:dischargeDispositionCode")
+    assert_equal 1, discharge_disposition_node.count
+    assert_equal "428361000124107", discharge_disposition_node.xpath("./@code").inner_text
 
     # negation rationale
+    # TODO
 
     # principal diagnosis
+    principal_diagnosis_node = encounter_performed_node.xpath("./xmlns:act/xmlns:entryRelationship/xmlns:encounter/xmlns:entryRelationship/xmlns:observation/xmlns:templateId[@root=\"2.16.840.1.113883.10.20.24.3.152\"]/parent::xmlns:observation")
+    assert_equal 1, principal_diagnosis_node.count
+    assert_equal "105401000119101", principal_diagnosis_node.xpath("./xmlns:value/@code").inner_text
 
     # code
+    code_node = encounter_performed_node.xpath("./xmlns:act/xmlns:entryRelationship/xmlns:encounter/xmlns:code")
+    assert_equal 1, code_node.count
+    assert_equal "12843005", code_node.xpath("./@code").inner_text
 
     # facility locations
-
-
+    facility_locations_nodes = encounter_performed_node.xpath("./xmlns:act/xmlns:entryRelationship/xmlns:encounter/xmlns:participant/xmlns:templateId[@root=\"2.16.840.1.113883.10.20.24.3.100\"]/parent::xmlns:participant")
     debugger
-    @initialized = true
+    assert_equal 2, facility_locations_nodes.count
+
+    emergency_department = facility_locations_nodes.xpath("./xmlns:participantRole/xmlns:code[@code=\"4525004\"]/parent::xmlns:participantRole/parent::xmlns:participant")
+    assert emergency_department != nil
+    assert "20120723080000", emergency_department.xpath("./xmlns:time/xmlns:low").inner_text
+    assert "20120723090000", emergency_department.xpath("./xmlns:time/xmlns:high").inner_text
+
+    office_visit = facility_locations_nodes.xpath("./xmlns:participantRole/xmlns:code[@code=\"185463005\"]/parent::xmlns:participantRole/parent::xmlns:participant")
+    assert office_visit != nil
+    assert "20120723090000", emergency_department.xpath("./xmlns:time/xmlns:low").inner_text
+    assert "20120723100000", emergency_department.xpath("./xmlns:time/xmlns:high").inner_text
   end
 
   def get_entry_xpath(qrda_oid)
