bca7581848e2f2f65c1726ce87ed92a3a5bc7eea
Lizzie DeYoung <edeyoung@mitre.org>
2018-08-08 12:33:39 -0400

    removed unneeded require statement.

--- test/unit/export/cat_1_qrda_r5_test.rb
+++ test/unit/export/cat_1_qrda_r5_test.rb
@@ -1,6 +1,5 @@
 require 'test_helper'
 require 'pry'
-require 'minitest/hooks/default'
 
 class Cat1TestQRDAR5 < Minitest::Test
   include HealthDataStandards::Export::Helper::Cat1ViewHelper
@@ -708,6 +707,7 @@ class Cat1TestQRDAR5 < Minitest::Test
       assert_equal "day", frequency_node.xpath("./@unit").inner_text
 
       # dosage
+      # dosage value entered as 5 but shows as 1 due to use of rxnorm code, which is precoordinated
       dosage_node = medication_order_node1.xpath("./xmlns:substanceAdministration/xmlns:doseQuantity")
       assert_equal 1, dosage_node.count
       assert_equal "5", dosage_node.xpath("./@value").inner_text
