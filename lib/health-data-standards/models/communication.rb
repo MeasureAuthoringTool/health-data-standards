92dca6a6f4771654a32b2699dbb28021146a7795
Lizzie DeYoung <edeyoung@mitre.org>
2018-08-10 13:23:26 -0400

    added new attributes

--- lib/health-data-standards/models/communication.rb
+++ lib/health-data-standards/models/communication.rb
@@ -1,3 +1,13 @@
 class Communication < Entry
   field :direction,           type: String
+  # QDM 5.4 merges all communication types.
+  # The following attributes were added for QDM 5.4
+  # * category
+  # * sender
+  # * recipient
+  # * medium
+  field :category,            type: Hash
+  field :sender,              type: Hash
+  field :recipient,           type: Hash
+  field :medium,              type: Hash
 end
