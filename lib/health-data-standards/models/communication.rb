e74359ec4d06d002015543e6d8bfe09c9a406db7
Luke Osborne <lwosborne@mitre.org>
2018-08-14 13:32:25 -0400

    add deprecated comments for communication direction

--- lib/health-data-standards/models/communication.rb
+++ lib/health-data-standards/models/communication.rb
@@ -1,5 +1,7 @@
 class Communication < Entry
+  # :direction is deprecated as of QDM 5.4
   field :direction,           type: String
+
   # QDM 5.4 merges all communication types.
   # The following attributes were added for QDM 5.4
   # * category
