02a82379c3f6cd6be25b783542e2df3b96357835
Matt Mayer <mmayer@mitre.org>
2018-09-26 15:05:32 -0400

    Remove class component type

--- lib/hqmf-model/types.rb
+++ lib/hqmf-model/types.rb
@@ -453,61 +453,27 @@ module HQMF
       check_equality(self,other)
     end
   end
-  
-  class Component
-    include HQMF::Conversion::Utilities
-    # From QDM 5.02 "A Component attribute includes a code and result"
-    # "for laboratorytest performed includes optional reference range high and reference range low"
-    attr_accessor :type, :code, :result, :range
-    
-    def initialize(type, code, value, unit, code_list_id, title, low = nil, high = nil)
-      @type = type || 'CMP'
-      @code = HQMF::Coded.new(@type, nil, nil, code_list_id, title)
-      @result = HQMF::Value.new(@type,unit,value,nil,nil,nil)
-      if(!high.nil? || !low.nil?)
-        # TODO: Determing if IVL_PQ (interval of physical quantity) is correct hl7 datatype for this
-        #@range = HQMF::Range.new('IVL_PQ', low, high, nil)
-      else
-        @range = nil
-      end
-    end
-      
-    def self.from_json(json)
-      code = json['code'] if json['code']
-      HQMF::Component.new(json["type"], code, json['value'], json['unit'], json['code_list_id'], json['title'])
-    end
 
-    def to_json
-      hash = build_hash(self, [:type])
-      hash['code'] = @code.to_json
-      hash['result'] = @result.to_json
-      # TODO: add range support for laboratorytest as specified by 5.01
-      #hash[:range] = @range.to_json if @range
-      hash
-    end  
-  end
-  
   class Ratio
     include HQMF::Conversion::Utilities
     attr_accessor :type, :numerator, :denominator
-    
+
     # @param [String] type
     # @param [Value] numerator
     # @param [Value] denominator
-  
     def initialize(type, numerator, denominator)
       @type = type || 'RT'
       @numerator = numerator
       @denominator = denominator
     end
-    
+
     def self.from_json(json)
       type = json["type"] if json["type"]
       numerator = json["numerator"] if json["numerator"]
       denominator = json["denominator"] if json["denominator"]
       HQMF::Ratio.new(type, numerator, denominator)
     end
-  
+
     def numerator?
       @numerator
     end
@@ -519,11 +485,10 @@ module HQMF
     def to_json
       build_hash(self, [:type, :numerator, :denominator])
     end
-    
+
     def ==(other)
       check_equality(self, other)
     end
-    
   end
   
 end
