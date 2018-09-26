f51fbb753b43f1c64efe0ad2ede0c12634d0225a
Matt Mayer <mmayer@mitre.org>
2018-09-26 15:19:44 -0400

    Remove unused id file

--- lib/health-data-standards/models/id.rb
+++
@@ -1,8 +0,0 @@
-class Id
-    include Mongoid::Document
-    include Mongoid::Attributes::Dynamic
-    embedded_in :entry
-    field :naming_system, type: String
-    field :id_value, type: String
- end
-  
