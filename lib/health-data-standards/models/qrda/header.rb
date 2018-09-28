03162d8b398e9b166456aa053e8d4d22b1d188e8
Giriraj Kashyap <giriraj.kashyapa@gmail.com>
2018-09-28 12:19:44 -0500

    Add Information Recipient to Header

--- lib/health-data-standards/models/qrda/header.rb
+++ lib/health-data-standards/models/qrda/header.rb
@@ -6,6 +6,8 @@ module Qrda
     embeds_many :authors
     embeds_one :custodian
     embeds_one :legal_authenticator
+    embeds_one :information_recipient
+    embeds_one :participant
     embeds_many :performers, class_name: "Provider"
 
     field  :time , type: DateTime, default: Time.now
