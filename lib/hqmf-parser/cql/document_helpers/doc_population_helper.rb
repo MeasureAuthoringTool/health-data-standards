5f18ce8877472844702cdab4da7c5d7746b25571
Matt Mayer <mayerm94@gmail.com>
2018-10-10 09:46:02 -0400

    Update xpath of observ param (#610)
    * Update xpath of where the measure observation function parameter is taken from
    
    * Add tests and fixtures for CV measure parsing

--- lib/hqmf-parser/cql/document_helpers/doc_population_helper.rb
+++ lib/hqmf-parser/cql/document_helpers/doc_population_helper.rb
@@ -40,7 +40,11 @@ module HQMF2CQL
           # The at_xpath(...).values returns an array of a single element.
           # The match returns an array and since we don't want the double quotes we take the second element
           cql_define_function[:function_name] = entry.at_xpath("*/cda:measureObservationDefinition/cda:value/cda:expression").values.first.match('\\"([A-Za-z0-9 ]+)\\"')[1]
-          cql_define_function[:parameter] = entry.at_xpath("*/cda:measureObservationDefinition/cda:component/cda:criteriaReference/cda:id").attributes['extension'].value.match('\\"([A-Za-z0-9 ]+)\\"')[1]
+          # The criteria_reference_id is the id of the measurePopulationCriteria that should be used for this observation function
+          measure_population_id = entry.at_xpath("*/cda:measureObservationDefinition/cda:component/cda:criteriaReference/cda:id").attributes['root'].value
+          # Get the name of the parameter to the  observation function within the measurePopulationCriteria section
+          cql_define_function[:parameter] = @doc.at_xpath("cda:QualityMeasureDocument/cda:component/cda:populationCriteriaSection/cda:component/cda:measurePopulationCriteria/cda:id[@root = \"#{measure_population_id}\"]/../cda:precondition/cda:criteriaReference/cda:id").attributes['extension'].value.match('\\"([A-Za-z0-9 ]+)\\"')[1]
+
           @observations << cql_define_function
         end
       end
